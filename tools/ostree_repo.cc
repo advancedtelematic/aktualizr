ffa7a78bbb42daf5b07e9433cbcdfe39fa08d49a
Phil Wise <phil@advancedtelematic.com>
2017-03-24 12:14:33 +0100

    Validate the OSTree repo is in archive-z2 format
    Fixes PRO-2766

--- tools/ostree_repo.cc
+++ tools/ostree_repo.cc
@@ -1,5 +1,6 @@
 #include <boost/filesystem.hpp>
 #include <boost/foreach.hpp>
+#include <boost/property_tree/ini_parser.hpp>
 #include <iomanip>
 #include <iostream>
 #include <sstream>
@@ -11,11 +12,33 @@
 
 using std::string;
 namespace fs = boost::filesystem;
+namespace pt = boost::property_tree;
 
 bool OSTreeRepo::LooksValid() const {
   fs::path objects_dir(root_ + "/objects");
   fs::path refs_dir(root_ + "/refs");
-  return fs::exists(objects_dir) && fs::exists(refs_dir);
+  fs::path config_file(root_ + "/config");
+  if (fs::is_directory(objects_dir) && fs::is_directory(refs_dir) &&
+      fs::is_regular(config_file)) {
+    pt::ptree config;
+    try {
+      pt::read_ini(config_file.string(), config);
+      if (config.get<string>("core.mode") != "archive-z2") {
+        LOG_WARNING << "OSTree repo is not in archive-z2 format";
+        return false;
+      } else {
+        return true;
+      }
+    } catch (const pt::ini_parser_error &error) {
+      LOG_WARNING << "Couldn't parse OSTree config file: " << config_file;
+      return false;
+    } catch (const pt::ptree_error &error) {
+      LOG_WARNING << "Could not find core.mode in OSTree config file";
+      return false;
+    }
+  } else {
+    return false;
+  }
 }
 
 OSTreeObject::ptr OSTreeRepo::GetObject(const uint8_t sha256[32]) const {
