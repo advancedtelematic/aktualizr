01f5ac2e4ac415ec1713af8db07f505dc846a640
Phil Wise <phil@advancedtelematic.com>
2017-05-26 14:25:07 +0200

    Reformat with clang-format

--- src/ostree.cc
+++ src/ostree.cc
@@ -22,7 +22,7 @@ OstreeSysroot *Ostree::LoadSysroot(const std::string &path) {
   return sysroot;
 }
 
-OstreeDeployment *Ostree::getBootedDeployment(const std::string& path) {
+OstreeDeployment *Ostree::getBootedDeployment(const std::string &path) {
   OstreeSysroot *sysroot = Ostree::LoadSysroot(path);
   OstreeRepo *repo = NULL;
   OstreeDeployment *booted_deployment = NULL;
@@ -170,7 +170,8 @@ data::InstallOutcome OstreePackage::install(const data::PackageManagerCredential
   return data::InstallOutcome(data::OK, "Installation succesfull");
 }
 
-OstreeBranch OstreeBranch::getCurrent(const std::string &ecu_serial, const std::string &branch, const std::string &ostree_sysroot, const std::string &ostree_os) {
+OstreeBranch OstreeBranch::getCurrent(const std::string &ecu_serial, const std::string &branch,
+                                      const std::string &ostree_sysroot, const std::string &ostree_os) {
   OstreeDeployment *booted_deployment = Ostree::getBootedDeployment(ostree_sysroot);
 
   if (!booted_deployment)
@@ -207,7 +208,8 @@ Json::Value OstreePackage::toEcuVersion(const Json::Value &custom) {
   return value;
 }
 
-OstreePackage OstreePackage::getEcu(const std::string &ecu_serial, const std::string &ostree_sysroot, const std::string &ostree_os) {
+OstreePackage OstreePackage::getEcu(const std::string &ecu_serial, const std::string &ostree_sysroot,
+                                    const std::string &ostree_os) {
   if (boost::filesystem::exists(NEW_PACKAGE)) {
     std::ifstream path_stream(NEW_PACKAGE.c_str());
     std::string json_content((std::istreambuf_iterator<char>(path_stream)), std::istreambuf_iterator<char>());
