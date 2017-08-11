a0715f83e47b921c7471ab9643e885ca28f9e31c
Patrick Vacek <patrickvacek@gmail.com>
2017-08-11 16:44:15 +0200

    Specify ports to allow parallel uptane vector test runs.
    This was only a problem for me when running locally with
    CTEST_PARALLEL_LEVEL > 1, but it was annoying and made me concerned that
    the problem could crop up in ugly ways in other places.

--- tests/run_vector_tests.sh
+++ tests/run_vector_tests.sh
@@ -9,14 +9,18 @@ fi
 
 pip install -r "$1/requirements.txt"
 $1/generator.py -t uptane --signature-encoding base64 -o vectors --cjson json-subset
-$1/server.py -t uptane --signature-encoding base64 &
+if [ "$2" == "valgrind" ]; then
+  $1/server.py -t uptane --signature-encoding base64 -P 8081 &
+else
+  $1/server.py -t uptane --signature-encoding base64 -P 8080 &
+fi
 sleep 3
 trap 'kill %1' EXIT
 
 if [ "$2" == "valgrind" ]; then
-    valgrind --track-origins=yes --show-possibly-lost=no --error-exitcode=1 --suppressions=$1/../aktualizr.supp ./aktualizr_uptane_vector_tests vectors/vector-meta.json
+    valgrind --track-origins=yes --show-possibly-lost=no --error-exitcode=1 --suppressions=$1/../aktualizr.supp ./aktualizr_uptane_vector_tests vectors/vector-meta.json 8081
 else
-    ./aktualizr_uptane_vector_tests vectors/vector-meta.json
+    ./aktualizr_uptane_vector_tests vectors/vector-meta.json 8080
 fi
 
 RES=$?
