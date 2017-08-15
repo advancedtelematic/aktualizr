2bdc1137b0bfe5509a086d585ea454893dd75136
Patrick Vacek <patrickvacek@gmail.com>
2017-08-15 11:13:18 +0200

    Get OS-assigned open port instead of hardcoding.

--- tests/run_vector_tests.sh
+++ tests/run_vector_tests.sh
@@ -2,25 +2,28 @@
 set -e
 
 if [ ! -f venv/bin/activate ]; then
-virtualenv -p python3 venv
+  virtualenv -p python3 venv
 fi
 
 . venv/bin/activate
 
 pip install -r "$1/requirements.txt"
+
+PORT=`$1/../get_open_port.py`
+
 $1/generator.py -t uptane --signature-encoding base64 -o vectors --cjson json-subset
 if [ "$2" == "valgrind" ]; then
-  $1/server.py -t uptane --signature-encoding base64 -P 8081 &
+  $1/server.py -t uptane --signature-encoding base64 -P $PORT &
 else
-  $1/server.py -t uptane --signature-encoding base64 -P 8080 &
+  $1/server.py -t uptane --signature-encoding base64 -P $PORT &
 fi
 sleep 3
 trap 'kill %1' EXIT
 
 if [ "$2" == "valgrind" ]; then
-    valgrind --track-origins=yes --show-possibly-lost=no --error-exitcode=1 --suppressions=$1/../aktualizr.supp ./aktualizr_uptane_vector_tests vectors/vector-meta.json 8081
+    valgrind --track-origins=yes --show-possibly-lost=no --error-exitcode=1 --suppressions=$1/../aktualizr.supp ./aktualizr_uptane_vector_tests vectors/vector-meta.json $PORT
 else
-    ./aktualizr_uptane_vector_tests vectors/vector-meta.json 8080
+    ./aktualizr_uptane_vector_tests vectors/vector-meta.json $PORT
 fi
 
 RES=$?
