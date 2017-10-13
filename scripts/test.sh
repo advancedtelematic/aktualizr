866505f1b05b5c994556e3a84dbb541011e81ba6
Phil Wise <phil@advancedtelematic.com>
2017-10-13 15:39:41 +0200

    Remove dependency between qa and package targets
    This isn't supported in cmake, and fails with the Makefile generator.  It did
    work with the ninga generator, though.
    
    https://public.kitware.com/Bug/view.php?id=8438

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,5 +9,5 @@ make -j8
 if [ -n "$BUILD_ONLY" ]; then
   echo "Skipping test run because of BUILD_ONLY environment variable"
 else
-  CTEST_OUTPUT_ON_FAILURE=1 CTEST_PARALLEL_LEVEL=5 make -j6 check-full
+  CTEST_OUTPUT_ON_FAILURE=1 CTEST_PARALLEL_LEVEL=5 make -j6 check-full package
 fi
