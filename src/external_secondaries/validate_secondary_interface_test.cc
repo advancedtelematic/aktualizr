8ae2b8af6658847f416c419df96bfbc856bfe34b
Patrick Vacek <patrickvacek@gmail.com>
2017-12-18 09:12:27 +0100

    Format validate_secondary_interface_test.cc.

--- src/external_secondaries/validate_secondary_interface_test.cc
+++ src/external_secondaries/validate_secondary_interface_test.cc
@@ -1,7 +1,7 @@
-#include <regex>
 #include <gtest/gtest.h>
 #include <boost/program_options.hpp>
 #include <boost/shared_ptr.hpp>
+#include <regex>
 
 namespace bpo = boost::program_options;
 
@@ -19,7 +19,7 @@ std::string exec(const std::string &cmd) {
   return result;
 }
 
-bool isVersionGood(const std::string &output){
+bool isVersionGood(const std::string &output) {
   std::string version = "1";
   return (output == version || output == version + "\n");
 }
@@ -34,13 +34,13 @@ TEST(ECUInterface, check_loglevel) {
   EXPECT_TRUE(isVersionGood(exec(cmd)));
 }
 
-bool isECUListValid(const std::string &output){
+bool isECUListValid(const std::string &output) {
   std::smatch ecu_match;
   std::regex ecu_regex("\\w+(?: \\w+)?\\n?");
 
   unsigned int matched_symbols = 0;
   std::string::const_iterator search_start(output.cbegin());
-  while(std::regex_search(search_start, output.cend(), ecu_match, ecu_regex)) {
+  while (std::regex_search(search_start, output.cend(), ecu_match, ecu_regex)) {
     matched_symbols += ecu_match.length();
     search_start += ecu_match.position() + ecu_match.length();
   }
@@ -58,30 +58,30 @@ TEST(ECUInterface, check_list_ecus_loglevel) {
 }
 
 TEST(ECUInterface, install_good) {
-
   std::smatch ecu_match;
   std::regex ecu_regex("(\\w+) ?(\\w+)?\\n?");
   std::string output = exec(target + " list-ecus");
 
   EXPECT_TRUE(std::regex_search(output, ecu_match, ecu_regex));
-  std::string cmd = target + " install-software  --firmware "+ firmware +" --hardware-identifier " + ecu_match[1].str();
+  std::string cmd =
+      target + " install-software  --firmware " + firmware + " --hardware-identifier " + ecu_match[1].str();
 
-  if (ecu_match.size() == 3){
-    cmd +=  std::string(" --ecu-identifier ") + ecu_match[2].str();
+  if (ecu_match.size() == 3) {
+    cmd += std::string(" --ecu-identifier ") + ecu_match[2].str();
   }
   EXPECT_EQ(system(cmd.c_str()), 0);
 }
 
 TEST(ECUInterface, install_bad) {
   std::string cmd = target +
-                    " install-software  --firmware somepath --hardware-identifier unexistent"
+                    " install-software --firmware somepath --hardware-identifier unexistent"
                     " --ecu-identifier unexistentserial";
   EXPECT_NE(system(cmd.c_str()), 0);
 }
 
 TEST(ECUInterface, install_bad_good_firmware) {
-  std::string cmd = target +
-                    " install-software  --firmware "+ firmware +" --hardware-identifier unexistent"
+  std::string cmd = target + " install-software --firmware " + firmware +
+                    " --hardware-identifier unexistent"
                     " --ecu-identifier unexistentserial";
   EXPECT_NE(system(cmd.c_str()), 0);
 }
@@ -91,21 +91,22 @@ int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   bpo::options_description description("aktualizr-validate-secondary-interface command line options");
   bpo::variables_map vm;
+  // clang-format off
   description.add_options()
     ("target", bpo::value<std::string>(&target)->required(), "target executable")
     ("firmware", bpo::value<std::string>(&firmware)->required(), "firmware to install");
-    bpo::basic_parsed_options<char> parsed_options =
-        bpo::command_line_parser(argc, argv).options(description).run();
-    try{
-      bpo::store(parsed_options, vm);
-      bpo::notify(vm);
-    } catch (const bpo::required_option &ex) {
-      std::cout << ex.what() << std::endl << description;
-      return EXIT_FAILURE;
-    } catch (const bpo::error &ex) {
-      std::cout << ex.what() << std::endl << description;
-      return EXIT_FAILURE;
-    }
+  // clang-format on
+  bpo::basic_parsed_options<char> parsed_options = bpo::command_line_parser(argc, argv).options(description).run();
+  try {
+    bpo::store(parsed_options, vm);
+    bpo::notify(vm);
+  } catch (const bpo::required_option &ex) {
+    std::cout << ex.what() << std::endl << description;
+    return EXIT_FAILURE;
+  } catch (const bpo::error &ex) {
+    std::cout << ex.what() << std::endl << description;
+    return EXIT_FAILURE;
+  }
 
   return RUN_ALL_TESTS();
 }
