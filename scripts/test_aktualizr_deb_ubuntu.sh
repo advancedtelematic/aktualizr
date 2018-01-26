7078c4a23ea5ff03980d31ad7ab0e68948956c9e
Laurent Bonnans <laurent.bonnans@here.com>
2018-01-26 16:24:03 +0100

    Add libprotobuf to dependencies for apt install test

--- scripts/test_aktualizr_deb_ubuntu.sh
+++ scripts/test_aktualizr_deb_ubuntu.sh
@@ -4,6 +4,6 @@ set -e
 if [ "$1" == "Dockerfile.16.04" ]; then
   echo "Building docker for test deb package inside it."
   docker build -t advancedtelematic/aktualizr_ubuntu_test -f Dockerfile.test.16.04 .
-  echo "Running docker conatiner with debian package inside."
+  echo "Running docker container with debian package inside."
   docker run -v /persistent:/persistent -t advancedtelematic/aktualizr_ubuntu_test
 fi
