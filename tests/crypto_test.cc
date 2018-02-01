9ba69daefb7d4d4b3a50a69f40f505316c511756
Anton Gerasimov <anton.gerasimov@here.com>
2018-02-01 13:11:15 +0100

    Add P11EngineGuard to count existing P11 users

--- tests/crypto_test.cc
+++ tests/crypto_test.cc
@@ -53,13 +53,13 @@ TEST(crypto, sign_verify_rsa_p11) {
   config.pass = "1234";
   config.uptane_key_id = "03";
 
-  P11Engine p11(config);
+  P11EngineGuard p11(config);
   std::string text = "This is text for sign";
   std::string key_content;
-  EXPECT_TRUE(p11.readUptanePublicKey(&key_content));
+  EXPECT_TRUE(p11->readUptanePublicKey(&key_content));
   PublicKey pkey(key_content, "rsa");
-  std::string private_key = p11.getUptaneKeyId();
-  std::string signature = Utils::toBase64(Crypto::RSAPSSSign(p11.getEngine(), private_key, text));
+  std::string private_key = p11->getUptaneKeyId();
+  std::string signature = Utils::toBase64(Crypto::RSAPSSSign(p11->getEngine(), private_key, text));
   bool signe_is_ok = Crypto::VerifySignature(pkey, signature, text);
   EXPECT_TRUE(signe_is_ok);
 }
@@ -70,11 +70,11 @@ TEST(crypto, generate_rsa_keypair_p11) {
   config.pass = "1234";
   config.uptane_key_id = "05";
 
-  P11Engine p11(config);
+  P11EngineGuard p11(config);
   std::string key_content;
-  EXPECT_FALSE(p11.readUptanePublicKey(&key_content));
-  EXPECT_TRUE(p11.generateUptaneKeyPair());
-  EXPECT_TRUE(p11.readUptanePublicKey(&key_content));
+  EXPECT_FALSE(p11->readUptanePublicKey(&key_content));
+  EXPECT_TRUE(p11->generateUptaneKeyPair());
+  EXPECT_TRUE(p11->readUptanePublicKey(&key_content));
 }
 
 TEST(crypto, certificate_pkcs11) {
@@ -82,10 +82,10 @@ TEST(crypto, certificate_pkcs11) {
   p11_conf.module = TEST_PKCS11_MODULE_PATH;
   p11_conf.pass = "1234";
   p11_conf.tls_clientcert_id = "01";
-  P11Engine p11(p11_conf);
+  P11EngineGuard p11(p11_conf);
 
   std::string cert;
-  bool res = p11.readTlsCert(&cert);
+  bool res = p11->readTlsCert(&cert);
   EXPECT_TRUE(res);
   if (!res) return;
 
--- tests/uptane_test.cc
+++ tests/uptane_test.cc
@@ -995,6 +995,8 @@ TEST(Uptane, Pkcs11Provision) {
 #ifndef __NO_MAIN__
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
+
+  logger_init();
   logger_set_threshold(boost::log::trivial::trace);
   return RUN_ALL_TESTS();
 }
