0109bf217325771ab6e54a11a3640c929c176eea
Laurent Bonnans <laurent.bonnans@here.com>
2018-02-14 15:02:47 +0100

    Clean up Dockerfile confusion
    - Dockerfile.16.04 should be Dockerfile.noostree (16.04 = xenial = used in
    main Dockerfile)
    - Dockerfile.test.16.04 should be Dockerfile.test-install.xenial

--- scripts/test_aktualizr_deb_ubuntu.sh
+++ scripts/test_aktualizr_deb_ubuntu.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-if [ "$1" == "Dockerfile.16.04" ]; then
+if [ "$1" == "Dockerfile.noostree" ]; then
   echo "Building docker for test deb package inside it."
-  docker build -t advancedtelematic/aktualizr_ubuntu_test -f Dockerfile.test.16.04 .
+  docker build -t advancedtelematic/aktualizr_ubuntu_test -f Dockerfile.test-install.xenial .
   echo "Running docker conatiner with debian package inside."
   docker run -v /persistent:/persistent -t advancedtelematic/aktualizr_ubuntu_test
 fi
