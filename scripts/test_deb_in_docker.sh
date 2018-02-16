dccafd10a93b2c127bf2c2c2bb52427b8291617a
Patrick Vacek <patrickvacek@gmail.com>
2018-02-16 11:27:26 +0100

    Rename garage-deploy deb files for clarity.
    Now that we also test aktualizr deb installation, it's worth specifying
    that the old mechanism was testing garage-deploy.

--- scripts/test_deb_in_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-if [ "$1" == "Dockerfile.deb-stable" ]; then
-  echo "Building docker for test deb package inside it."
-  docker build -t advancedtelematic/garage_deploy_test -f src/sota_tools/Dockerfile.debian.stable .
-  echo "Running docker conatiner with debian package inside."
-  docker run -v /persistent:/persistent -t advancedtelematic/garage_deploy_test
-fi
