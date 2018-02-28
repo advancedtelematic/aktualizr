89e2c09d1f40d6d708a165d646aca5221f179a73
Laurent Bonnans <laurent.bonnans@here.com>
2018-02-28 13:47:34 +0100

    Add a test for a current bug in sql_utils

---
+++ tests/sql_utils_test.cc
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+
+#include "sql_utils.h"
+#include "utils.h"
+
+TEST(sql_utils, PrepareRvalue) {
+  TemporaryDirectory temp_dir;
+  SQLite3Guard db((temp_dir.Path() / "test.db").c_str());
+
+  db.exec("CREATE TABLE example(example TEXT);", NULL, NULL);
+  // the arguments used in prepareStatement should last for the subsequent
+  // sqlite calls (eg: `.step()`)
+  auto statement = db.prepareStatement<std::string>("INSERT INTO example(example) VALUES (?);", temp_dir.PathString());
+  EXPECT_EQ(statement.step(), SQLITE_DONE);
+}
+
+#ifndef __NO_MAIN__
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  logger_init();
+  logger_set_threshold(boost::log::trivial::trace);
+
+  return RUN_ALL_TESTS();
+}
+#endif
