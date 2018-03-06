89d0f43e6e2858b70f4234d940efb137514926ea
Anton Gerasimov <anton@advancedtelematic.com>
2018-03-06 15:33:55 +0100

    Reduce parallelism in hope to make travis happy

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -7,7 +7,7 @@ mkdir -p build-coverage
 cd build-coverage
 cmake -DBUILD_OSTREE=ON -DBUILD_SOTA_TOOLS=ON -DBUILD_DEB=ON -DBUILD_P11=ON -DBUILD_WITH_CODE_COVERAGE=ON -DTEST_PKCS11_ENGINE_PATH="/usr/lib/engines/engine_pkcs11.so" -DTEST_PKCS11_MODULE_PATH="/usr/lib/softhsm/libsofthsm2.so" ../src
 make -j8
-CTEST_OUTPUT_ON_FAILURE=1 CTEST_PARALLEL_LEVEL=5 make -j6 coverage
+CTEST_OUTPUT_ON_FAILURE=1 CTEST_PARALLEL_LEVEL=3 make -j6 coverage
 cd ..
 if [ -n "$TRAVIS_COMMIT" ]; then
   bash <(curl -s https://codecov.io/bash -p src -s build-coverage)
