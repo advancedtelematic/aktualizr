8d9097fabbfb13a231d5e4543c47d8411286d778
Laurent Bonnans <laurent.bonnans@here.com>
2018-03-12 10:21:13 +0100

    Wrap socket fd in a Handle for RAII close

--- src/aktualizr_secondary/main.cc
+++ src/aktualizr_secondary/main.cc
@@ -101,14 +101,27 @@ void handle_follow_conn_messages(int con_fd, std::unique_ptr<sockaddr_storage> a
   LOG_INFO << "Connection closed with " << peer_name;
 }
 
-static int secondary_open_socket(const AktualizrSecondaryConfig &config) {
+struct SocketCloser {
+  void operator()(int * ptr) const {
+    close(*ptr);
+  }
+};
+
+using SocketHandle = std::unique_ptr<int, SocketCloser>;
+
+static SocketHandle secondary_open_socket(const AktualizrSecondaryConfig &config) {
   if (socket_activation::listen_fds(0) == 1) {
     LOG_INFO << "Using socket activation";
-    return socket_activation::listen_fds_start;
+    SocketHandle hdl(new int(socket_activation::listen_fds_start));
+    return std::move(hdl);
   }
 
   // manual socket creation
   int socket_fd = socket(AF_INET6, SOCK_STREAM, 0);
+  if (socket_fd < 0) {
+    throw std::runtime_error("socket creation failed");
+  }
+  SocketHandle hdl(new int(socket_fd));
   sockaddr_in6 sa;
 
   memset(&sa, 0, sizeof(sa));
@@ -117,24 +130,24 @@ static int secondary_open_socket(const AktualizrSecondaryConfig &config) {
   sa.sin6_addr = IN6ADDR_ANY_INIT;
 
   int v6only = 0;
-  if (setsockopt(socket_fd, IPPROTO_IPV6, IPV6_V6ONLY, &v6only, sizeof(v6only)) < 0) {
+  if (setsockopt(*hdl, IPPROTO_IPV6, IPV6_V6ONLY, &v6only, sizeof(v6only)) < 0) {
     throw std::runtime_error("setsockopt(IPV6_V6ONLY) failed");
   }
 
   int reuseaddr = 1;
-  if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &reuseaddr, sizeof(reuseaddr)) < 0) {
+  if (setsockopt(*hdl, SOL_SOCKET, SO_REUSEADDR, &reuseaddr, sizeof(reuseaddr)) < 0) {
     throw std::runtime_error("setsockopt(SO_REUSEADDR) failed");
   }
 
-  if (bind(socket_fd, reinterpret_cast<const sockaddr *>(&sa), sizeof(sa)) < 0) {
+  if (bind(*hdl, reinterpret_cast<const sockaddr *>(&sa), sizeof(sa)) < 0) {
     throw std::runtime_error("bind failed");
   }
 
-  if (listen(socket_fd, SOMAXCONN) < 0) {
+  if (listen(*hdl, SOMAXCONN) < 0) {
     throw std::runtime_error("listen failed");
   }
 
-  return socket_fd;
+  return std::move(hdl);
 }
 
 /*****************************************************************************/
@@ -175,25 +188,27 @@ int main(int argc, char *argv[]) {
   try {
     AktualizrSecondaryConfig config(secondary_config_path, commandline_map);
 
-    int socket_fd = secondary_open_socket(config);
+    SocketHandle socket_fd = secondary_open_socket(config);
     LOG_INFO << "Listening on port " << config.network.port;
 
     SecondaryPacket::ChanType channel;
 
     // listen for TCP connections
-    std::thread tcp_thread = std::thread([&channel, socket_fd]() {
+    std::thread tcp_thread = std::thread([&channel, &socket_fd]() {
       while (true) {
         std::unique_ptr<sockaddr_storage> peer_sa(new sockaddr_storage);
         socklen_t other_sasize = sizeof(*peer_sa);
         int con_fd;
 
-        if ((con_fd = accept(socket_fd, reinterpret_cast<sockaddr *>(peer_sa.get()), &other_sasize)) == -1) {
+        if ((con_fd = accept(*socket_fd, reinterpret_cast<sockaddr *>(peer_sa.get()), &other_sasize)) == -1) {
           break;
         }
 
         // One thread per connection
         std::thread(handle_follow_conn_messages, con_fd, std::move(peer_sa), std::ref(channel)).detach();
       }
+
+      channel.close();
     });
 
     // listen for messages
