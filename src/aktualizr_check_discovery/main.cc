923346f704e3b80d30c0fbfe95873239778fb288
Serhiy Stetskovych <patriotyk@gmail.com>
2018-03-27 15:53:55 +0300

    Add utility to check discovering

---
+++ src/aktualizr_check_discovery/main.cc
@@ -0,0 +1,79 @@
+#include <boost/program_options.hpp>
+#include <iostream>
+#include <string>
+
+#include "config.h"
+#include "uptane/secondaryfactory.h"
+#include "uptane/secondaryinterface.h"
+
+#include "ipsecondarydiscovery.h"
+#include "ipuptaneconnection.h"
+#include "ipuptaneconnectionsplitter.h"
+#include "logging.h"
+
+namespace po = boost::program_options;
+
+int main(int argc, char **argv) {
+  po::options_description desc("aktualizr_info command line options");
+  // clang-format off
+  desc.add_options()
+    ("help,h", "print usage")
+    ("config,c", po::value<std::string>(), "toml configuration file");
+  // clang-format on
+
+  try {
+    logger_init();
+    logger_set_threshold(boost::log::trivial::info);
+    po::variables_map vm;
+    po::basic_parsed_options<char> parsed_options = po::command_line_parser(argc, argv).options(desc).run();
+    po::store(parsed_options, vm);
+    po::notify(vm);
+    if (vm.count("help") != 0) {
+      std::cout << desc << '\n';
+      exit(EXIT_SUCCESS);
+    }
+
+    std::string sota_config_file = "/usr/lib/sota/sota.toml";
+    if (vm.count("config") != 0) {
+      sota_config_file = vm["config"].as<std::string>();
+    } else if (boost::filesystem::exists("/tmp/aktualizr_config_path")) {
+      sota_config_file = Utils::readFile("/tmp/aktualizr_config_path");
+    }
+
+    boost::filesystem::path sota_config_path(sota_config_file);
+    if (false == boost::filesystem::exists(sota_config_path)) {
+      std::cout << "configuration file " << boost::filesystem::absolute(sota_config_path) << " not found. Exiting."
+                << std::endl;
+      exit(EXIT_FAILURE);
+    }
+    Config config(sota_config_path.string());
+
+    IpSecondaryDiscovery ip_uptane_discovery{config.network};
+    auto discovered = ip_uptane_discovery.discover();
+    LOG_INFO << "Discovering finished";
+    LOG_INFO << "Found " << discovered.size() << " devices";
+    if (discovered.size()) {
+      LOG_INFO << "Trying to connect with founded devices and get public key";
+      IpUptaneConnection ip_uptane_connection(config.network.ipuptane_port);
+      IpUptaneConnectionSplitter ip_uptane_splitter(ip_uptane_connection);
+
+      std::vector<Uptane::SecondaryConfig>::const_iterator it;
+      for (it = discovered.begin(); it != discovered.end(); ++it) {
+        boost::shared_ptr<Uptane::SecondaryInterface> sec = Uptane::SecondaryFactory::makeSecondary(*it);
+        if (it->secondary_type == Uptane::kIpUptane) {
+          dynamic_cast<Uptane::IpUptaneSecondary *>(&(*sec))->connect(&ip_uptane_splitter);
+          auto public_key = sec->getPublicKey();
+          LOG_INFO << "Got public key from secondary: " << public_key.second;
+        }
+      }
+      exit(EXIT_SUCCESS);
+    }
+    LOG_INFO << "Exiting";
+
+  } catch (const po::error &o) {
+    std::cout << o.what() << std::endl;
+    std::cout << desc;
+    return EXIT_FAILURE;
+  }
+}
+// vim: set tabstop=2 shiftwidth=2 expandtab:
