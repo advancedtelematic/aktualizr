de17bc1cada111cbbce031b18331b7dd03e7b7bd
Laurent Bonnans <laurent.bonnans@here.com>
2018-03-27 10:55:59 +0200

    Add utility for creating simple archives (tar.gz)

--- src/utils.h
+++ src/utils.h
@@ -21,12 +21,13 @@ struct Utils {
   static std::string jsonToStr(const Json::Value &json);
   static std::string genPrettyName();
   static std::string readFile(const boost::filesystem::path &filename);
-  static std::string readFileFromArchive(std::istream &as, const std::string &filename);
   static void writeFile(const boost::filesystem::path &filename, const std::string &content,
                         bool create_directories = true);
   static void writeFile(const boost::filesystem::path &filename, const Json::Value &content,
                         bool create_directories = true);
   static void copyDir(const boost::filesystem::path &from, const boost::filesystem::path &to);
+  static std::string readFileFromArchive(std::istream &as, const std::string &filename);
+  static void writeArchive(const std::map<std::string, std::string> &entries, std::ostream &as);
   static Json::Value getHardwareInfo();
   static std::string getHostname();
   static std::string randomUuid();
