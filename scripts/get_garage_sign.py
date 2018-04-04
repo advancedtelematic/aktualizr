cfda2276195a18581c7d135ad31320add95657b4
Patrick Vacek <patrickvacek@gmail.com>
2018-04-04 14:45:47 +0200

    Input args for output directory and specific version.
    Also sort by garage-sign name/version instead of LastModified. It's
    easier to search and the default is more likely to be correct.

--- scripts/get_garage_sign.py
+++ scripts/get_garage_sign.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import hashlib
 import os.path
 import requests
@@ -7,8 +8,18 @@ import shutil
 import sys
 import xml.etree.ElementTree as ET
 
+from pathlib import Path
+
 
 def main():
+    parser = argparse.ArgumentParser(description='Download a specific or the latest version of garage-sign')
+    parser.add_argument('-n', '--name', help='specific version to download')
+    parser.add_argument('-o', '--output', type=Path, default=Path('.'), help='download directory')
+    args = parser.parse_args()
+    if not args.output.exists():
+        print('Error: specified output directory ' + args.output + ' does not exist!')
+        return 1
+
     r = requests.get('https://ats-tuf-cli-releases.s3-eu-central-1.amazonaws.com')
     if r.status_code != 200:
         print('Error: unable to request index!')
@@ -20,13 +31,19 @@ def main():
     versions = dict()
     for i in (i for i in items if i.find(ns + 'Key').text[0:4] == 'cli-'):
         # ETag is md5sum.
-        versions[i.find(ns + 'LastModified').text] = (i.find(ns + 'Key').text, i.find(ns + 'ETag').text[1:-1])
-    latest = sorted(versions)[-1]
-    name = versions[latest][0]
-    md5_hash = versions[latest][1]
+        versions[i.find(ns + 'Key').text] = (i.find(ns + 'LastModified').text, i.find(ns + 'ETag').text[1:-1])
+    if args.name:
+        name = args.name
+        if name not in versions:
+            print('Error: ' + name + ' not found in tuf-cli releases.')
+            return 1
+    else:
+        name = sorted(versions)[-1]
+    path = args.output.joinpath(name)
+    md5_hash = versions[name][1]
     m = hashlib.md5()
-    if os.path.isfile(name):
-        if check_md5(name, m, md5_hash):
+    if os.path.isfile(path):
+        if check_md5(path, m, md5_hash):
             print(name + ' already present with valid md5 hash.')
             return 0
         else:
@@ -36,9 +53,9 @@ def main():
     if r2.status_code != 200:
         print('Error: unable to request file!')
         return 1
-    with open(name, mode='wb') as f:
+    with open(path, mode='wb') as f:
         shutil.copyfileobj(r2.raw, f)
-    if not check_md5(name, m, md5_hash):
+    if not check_md5(path, m, md5_hash):
         print('Error: md5 hash of ' + name + ' does not match expected value!')
         return 1
     else:
@@ -46,8 +63,8 @@ def main():
         return 0
 
 
-def check_md5(name, m, md5_hash):
-    with open(name, mode='rb') as f:
+def check_md5(path, m, md5_hash):
+    with open(path, mode='rb') as f:
         m.update(f.read())
     return m.hexdigest() == md5_hash
 
