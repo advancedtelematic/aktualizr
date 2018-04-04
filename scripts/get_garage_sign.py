eea4b4727f0d945d5c9f340bd2585cfde3bad205
Patrick Vacek <patrickvacek@gmail.com>
2018-04-04 14:05:29 +0200

    Don't download if already present with matching md5 hash.

--- scripts/get_garage_sign.py
+++ scripts/get_garage_sign.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import hashlib
+import os.path
 import requests
 import shutil
 import sys
@@ -23,19 +24,32 @@ def main():
     latest = sorted(versions)[-1]
     name = versions[latest][0]
     md5_hash = versions[latest][1]
+    m = hashlib.md5()
+    if os.path.isfile(name):
+        if check_md5(name, m, md5_hash):
+            print(name + ' already present with valid md5 hash.')
+            return 0
+        else:
+            print('md5 hash of local ' + name + ' doesn\'t match expected value, downloading from server...')
+
     r2 = requests.get('https://ats-tuf-cli-releases.s3-eu-central-1.amazonaws.com/' + name, stream=True)
     if r2.status_code != 200:
         print('Error: unable to request file!')
         return 1
     with open(name, mode='wb') as f:
         shutil.copyfileobj(r2.raw, f)
-    m = hashlib.md5()
+    if not check_md5(name, m, md5_hash):
+        print('Error: md5 hash of ' + name + ' does not match expected value!')
+        return 1
+    else:
+        print(name + ' successfully downloaded with valid md5 hash.')
+        return 0
+
+
+def check_md5(name, m, md5_hash):
     with open(name, mode='rb') as f:
         m.update(f.read())
-    if m.hexdigest() != md5_hash:
-        print('Error: md5 hash does not match!')
-        return 1
-    return 0
+    return m.hexdigest() == md5_hash
 
 
 if __name__ == '__main__':
