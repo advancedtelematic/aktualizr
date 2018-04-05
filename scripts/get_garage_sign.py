59bbe88992dbd5cb93dd709839bb86d91fbeeaa1
Patrick Vacek <patrickvacek@gmail.com>
2018-04-05 12:20:03 +0200

    Add --sha256 to get_garage_sign.
    Set in CMake with -DGARAGE_SIGN_SHA256=<hash>. If set, it will check
    that the tarball's hash matches. If not set when a specific version is
    requested, it throws a warning.

--- scripts/get_garage_sign.py
+++ scripts/get_garage_sign.py
@@ -15,11 +15,17 @@ from pathlib import Path
 def main():
     parser = argparse.ArgumentParser(description='Download a specific or the latest version of garage-sign')
     parser.add_argument('-n', '--name', help='specific version to download')
+    parser.add_argument('-s', '--sha256', help='expected hash of requested version')
     parser.add_argument('-o', '--output', type=Path, default=Path('.'), help='download directory')
     args = parser.parse_args()
     if not args.output.exists():
         print('Error: specified output directory ' + args.output + ' does not exist!')
         return 1
+    sha256_hash = args.sha256
+    if sha256_hash and not args.name:
+        print('Warning: sha256 hash specified without specifying a version.')
+    if args.name and not sha256_hash:
+        print('Warning: specific version requested without specifying the sha256 hash.')
 
     r = requests.get('https://ats-tuf-cli-releases.s3-eu-central-1.amazonaws.com')
     if r.status_code != 200:
@@ -47,15 +53,14 @@ def main():
         name = sorted(versions)[-1]
     path = args.output.joinpath(name)
     md5_hash = versions[name][1]
-    m = hashlib.md5()
-    if not path.is_file() or not check_md5(name, path, m, md5_hash):
+    if not path.is_file() or not check_hashes(name, path, md5_hash, sha256_hash):
         print('Downloading ' + name + ' from server...')
-        if download(name, path, m, md5_hash):
+        if download(name, path, md5_hash, sha256_hash):
             print(name + ' successfully downloaded and validated.')
         else:
             return 1
     else:
-        print(name + ' already present with valid md5 hash.')
+        print(name + ' already present and validated.')
 
     # Remove anything leftover inside the extracted directory.
     extract_path = args.output.joinpath('garage-sign')
@@ -68,27 +73,37 @@ def main():
     return 0
 
 
-def download(name, path, m, md5_hash):
+def download(name, path, md5_hash, sha256_hash):
     r2 = requests.get('https://ats-tuf-cli-releases.s3-eu-central-1.amazonaws.com/' + name, stream=True)
     if r2.status_code != 200:
         print('Error: unable to request file!')
         return False
     with path.open(mode='wb') as f:
         shutil.copyfileobj(r2.raw, f)
-    return check_md5(name, path, m, md5_hash)
+    return check_hashes(name, path, md5_hash, sha256_hash)
 
 
-def check_md5(name, path, m, md5_hash):
+def check_hashes(name, path, md5_hash, sha256_hash):
     if not tarfile.is_tarfile(str(path)):
         print('Error: ' + name + ' is not a valid tar archive!')
         return False
+    m = hashlib.md5()
+    if sha256_hash:
+        s = hashlib.sha256()
     with path.open(mode='rb') as f:
-        m.update(f.read())
-    if m.hexdigest() == md5_hash:
-        return True
-    else:
+        data = f.read()
+        m.update(data)
+        if sha256_hash:
+            s.update(data)
+    if m.hexdigest() != md5_hash:
         print('Error: md5 hash of ' + name + ' does not match expected value!')
+        print(m.hexdigest())
+        print(md5_hash)
         return False
+    if sha256_hash and s.hexdigest() != sha256_hash:
+        print('Error: sha256 hash of ' + name + ' does not match provided value!')
+        return False
+    return True
 
 
 if __name__ == '__main__':
