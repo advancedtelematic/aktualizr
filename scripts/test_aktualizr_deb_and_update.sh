bca59aab50d8d0870d48a6f176d3c9bd59ef4e91
Laurent Bonnans <laurent.bonnans@here.com>
2018-04-23 11:54:54 +0200

    Don't create root-owned files in debian install test
    It's problematic to create root-owned file in the build directory since
    Jenkins has trouble deleting them.
    
    Removing them afterwards is potentially flaky (in case of interrupted
    tests for example), best not to create them at all → run storage from
    `/tmp` but take provisioning data from `/persistent`

--- scripts/test_aktualizr_deb_and_update.sh
+++ scripts/test_aktualizr_deb_and_update.sh
@@ -1,16 +1,12 @@
 #! /bin/bash
 set -ex
 
-clean() {
-    rm -rf /persistent/prov_selfupdate
-}
-trap clean EXIT
-
 /persistent/selfupdate_server.py 8000&
 
 dpkg-deb -I /persistent/aktualizr.deb && dpkg -i /persistent/aktualizr.deb
 aktualizr --version | grep "$(cat /persistent/aktualizr-version)" && aktualizr-info
 
+mkdir -m 700 -p /tmp/aktualizr-storage
 aktualizr -c /persistent/selfupdate.toml --poll-once
 
 aktualizr --version | grep 2.0-selfupdate
