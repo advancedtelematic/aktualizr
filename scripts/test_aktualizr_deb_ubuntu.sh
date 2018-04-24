6bf3e960c673a031674ef72b6e4223e9bcf5d25a
Laurent Bonnans <laurent.bonnans@here.com>
2018-04-24 16:27:13 +0200

    Streamline some of the Travis logic
    Check what needs to run at the top instead of inside the scripts

--- scripts/test_aktualizr_deb_ubuntu.sh
+++ scripts/test_aktualizr_deb_ubuntu.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 set -ex
 
-if [ "$1" == "Dockerfile.noostree" ]; then
-  PKG_SRCDIR="${2:-/persistent}"
-  echo "Building docker for testing aktualizr deb package inside it."
-  docker build -t advancedtelematic/aktualizr_ubuntu_test -f Dockerfile.test-install.xenial .
-  echo "Running docker container with aktualizr debian package inside."
-  docker run -v "${PKG_SRCDIR}":/persistent -t advancedtelematic/aktualizr_ubuntu_test
-fi
+PKG_SRCDIR="${2:-/persistent}"
+echo "Building docker for testing aktualizr deb package inside it."
+docker build -t advancedtelematic/aktualizr_ubuntu_test -f Dockerfile.test-install.xenial .
+echo "Running docker container with aktualizr debian package inside."
+docker run -v "${PKG_SRCDIR}":/persistent -t advancedtelematic/aktualizr_ubuntu_test
