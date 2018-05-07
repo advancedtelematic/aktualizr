0dc9632c24d93894b4ce802eeda131121845ee6a
Laurent Bonnans <laurent.bonnans@here.com>
2018-05-07 15:37:01 +0200

    Use unique tag for garage deploy and aktualizr deb pkg tests
    To avoid future issues.
    Also call `docker run` with `--rm` to avoid accumulating dead containers

--- scripts/test_garage_deploy_deb.sh
+++ scripts/test_garage_deploy_deb.sh
@@ -3,7 +3,9 @@
 set -exuo pipefail
 
 PKG_SRCDIR="${1:-/persistent}"
+IMG_TAG=garage-deploy-$(cat /proc/sys/kernel/random/uuid)
+
 echo "Building docker for testing garage-deploy deb package inside it."
-docker build -t advancedtelematic/garage_deploy_test -f Dockerfile.garage-deploy.deb-stable .
+docker build -t "${IMG_TAG}" -f Dockerfile.garage-deploy.deb-stable .
 echo "Running docker container with garage-deploy debian package inside."
-docker run -v "${PKG_SRCDIR}":/persistent -t advancedtelematic/garage_deploy_test
+docker run --rm -v "${PKG_SRCDIR}":/persistent -t "${IMG_TAG}"
