998d504642c013850ca93bb57f5e559472751f27
Laurent Bonnans <laurent.bonnans@here.com>
2018-05-07 11:18:09 +0200

    Robustify tests/run_vector_tests.sh
    Make sure that the tuf-test-vectors server is running before running our
    tests. Bail out after 10 seconds

--- tests/run_vector_tests.sh
+++ tests/run_vector_tests.sh
@@ -11,17 +11,27 @@ fi
 python -m pip install wheel
 python -m pip install -r "$1/requirements.txt"
 
-PORT=`$1/../get_open_port.py`
+PORT=$("$1/../get_open_port.py")
 
-$1/generator.py -t uptane --signature-encoding base64 -o vectors --cjson json-subset
-$1/server.py -t uptane --signature-encoding base64 -P $PORT &
-sleep 3
+"$1/generator.py" -t uptane --signature-encoding base64 -o vectors --cjson json-subset
+"$1/server.py" -t uptane --signature-encoding base64 -P "$PORT" &
 trap 'kill %1' EXIT
 
+# wait for server to go up
+tries=0
+while ! curl -I -s -f "http://localhost:$PORT"; do
+    if [[ $tries -ge 10 ]]; then
+        echo "tuf-test-vectors server did not go up"
+        exit 1
+    fi
+    sleep 1
+    tries=$((tries+1))
+done
+
 if [ "$2" == "valgrind" ]; then
-    valgrind --track-origins=yes --show-possibly-lost=no --error-exitcode=1 --suppressions=$1/../aktualizr.supp ./aktualizr_uptane_vector_tests vectors/vector-meta.json $PORT
+    valgrind --track-origins=yes --show-possibly-lost=no --error-exitcode=1 --suppressions="$1/../aktualizr.supp" ./aktualizr_uptane_vector_tests vectors/vector-meta.json "$PORT"
 else
-    ./aktualizr_uptane_vector_tests vectors/vector-meta.json $PORT
+    ./aktualizr_uptane_vector_tests vectors/vector-meta.json "$PORT"
 fi
 
 RES=$?
