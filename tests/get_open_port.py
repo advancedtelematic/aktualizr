e79c793135b86567458340d72ff03d0985cbcdc7
Laurent Bonnans <laurent.bonnans@here.com>
2018-05-07 17:07:48 +0200

    Try to fix tuf-test-vectors port bind issue

--- tests/get_open_port.py
+++ tests/get_open_port.py
@@ -6,7 +6,8 @@ import socket
 
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('', 0))
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(('127.0.0.1', 0))
     port = s.getsockname()[1]
     print(port)
     s.close()
