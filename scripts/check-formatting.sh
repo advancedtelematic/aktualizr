4cf736b4f8f31b721915539eabda901c1c82407c
Laurent Bonnans <laurent.bonnans@here.com>
2018-05-17 14:44:35 +0200

    Make check-formatting.sh fail when file could not be found
    Also simplify the script in general and does not support running it on
    several files at once since it seems that we don't use this mode anymore

--- scripts/check-formatting.sh
+++ scripts/check-formatting.sh
@@ -2,15 +2,19 @@
 set -euo pipefail
 
 CLANG_FORMAT=$1
-shift
-FILES=$@
+SRC_FILE=$2
 
-DIFF=$(tempfile)
+DIFF=$(mktemp)
+cleanup() {
+    rm "$DIFF"
+}
+trap cleanup EXIT
+
 FAIL=0
-diff -u <(cat ${FILES}) <(${CLANG_FORMAT} -style=file ${FILES}) > ${DIFF} || FAIL=1
+diff -u "$SRC_FILE" <("$CLANG_FORMAT" -style=file "$SRC_FILE") > "$DIFF" || FAIL=1
 if [ ${FAIL} -eq 1 ]; then
-    echo "${CLANG_FORMAT} found unformatted code! Run 'make qa' before continuing."
-    cat ${DIFF}
+    echo "$CLANG_FORMAT found unformatted code! Run 'make qa' before continuing."
+    cat "$DIFF"
     exit 1
 fi
 
