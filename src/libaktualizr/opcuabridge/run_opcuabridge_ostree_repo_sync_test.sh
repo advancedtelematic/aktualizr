69486145fe1807a34b2394e8607673a7a9592fe9
Laurent Bonnans <laurent.bonnans@here.com>
2018-05-18 17:10:33 +0200

    Move opcuabridge integration test to its directory
    Plus, make it actually use valgrind. It needed a bit of reorganization
    in tests/

--- src/libaktualizr/opcuabridge/run_opcuabridge_ostree_repo_sync_test.sh
+++ src/libaktualizr/opcuabridge/run_opcuabridge_ostree_repo_sync_test.sh
@@ -3,6 +3,7 @@ set -e
 
 BIN_DIR=$1
 SOURCE_TESTS_DIR=$2
+VALGRIND=${3:-}  # $3 can be set to 'valgrind' (or the run-valgrind wrapper)
 ORIG_REPO=$SOURCE_TESTS_DIR/sota_tools/repo
 
 TEST_SRC_REPO=$(mktemp -d)
@@ -15,19 +16,11 @@ ostree init --mode=bare --repo=$TEST_DST_REPO
 
 PORT=`$SOURCE_TESTS_DIR/get_open_port.py`
 
-if [ "$3" == "valgrind" ]; then
-  valgrind --error-exitcode=1 --leak-check=yes --show-possibly-lost=yes \
-           --errors-for-leak-kinds=definite --suppressions=$SOURCE_TESTS_DIR/aktualizr.supp \
-           $BIN_DIR/opcuabridge-ostree-repo-sync-server $TEST_DST_REPO $PORT &
-else
-  $BIN_DIR/opcuabridge-ostree-repo-sync-server $TEST_DST_REPO $PORT &
-fi
+$VALGRIND $BIN_DIR/opcuabridge-ostree-repo-sync-server $TEST_DST_REPO $PORT &
 sleep 3
 trap 'kill %1' EXIT
 
-# $3 can be set to 'valgrind' (or the run-valgrind wrapper)
-$3 $BIN_DIR/opcuabridge-ostree-repo-sync-client $TEST_SRC_REPO $PORT
-
+$VALGRIND $BIN_DIR/opcuabridge-ostree-repo-sync-client $TEST_SRC_REPO $PORT
 
 ostree show --repo=$TEST_DST_REPO $TEST_REF > $TEST_DST_REPO/__ostree_show_$TEST_REF
 diff $TEST_SRC_REPO/__ostree_show_$TEST_REF $TEST_DST_REPO/__ostree_show_$TEST_REF
