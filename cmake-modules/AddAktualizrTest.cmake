dc2c0d4edc690af61775be554f9c27dce6c96c44
Laurent Bonnans <laurent.bonnans@here.com>
2018-06-07 15:33:10 +0200

    Fix lcov coverage reports
    * exclude tests, external and generated code
    * only compile our code with coverage compiler flags
    * do not force `-O0` on coverage
    * `--coverage` includes `-lgcov`, so get rid of explicit linking with
      gcov.
    
      From man:
    
        The option is a synonym for -fprofile-arcs -ftest-coverage (when
        compiling) and -lgcov

--- cmake-modules/AddAktualizrTest.cmake
+++ cmake-modules/AddAktualizrTest.cmake
@@ -6,21 +6,24 @@ function(add_aktualizr_test)
     add_executable(t_${AKTUALIZR_TEST_NAME} EXCLUDE_FROM_ALL ${AKTUALIZR_TEST_SOURCES} ${PROJECT_SOURCE_DIR}/tests/test_utils.cc)
     target_link_libraries(t_${AKTUALIZR_TEST_NAME} aktualizr_static_lib ${TEST_LIBS})
     target_include_directories(t_${AKTUALIZR_TEST_NAME} PUBLIC ${PROJECT_SOURCE_DIR}/tests)
+
     if(AKTUALIZR_TEST_PROJECT_WORKING_DIRECTORY)
         set(WD WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
     else()
         set(WD )
     endif()
-        # run tests under valgrind if the correct CMAKE_BUILD_TYPE is set
-        if(CMAKE_BUILD_TYPE MATCHES "Valgrind")
-            add_test(NAME test_${AKTUALIZR_TEST_NAME}
-                     COMMAND ${RUN_VALGRIND} ${CMAKE_CURRENT_BINARY_DIR}/t_${AKTUALIZR_TEST_NAME} ${AKTUALIZR_TEST_ARGS}
-                     ${WD})
-        else()
-            add_test(NAME test_${AKTUALIZR_TEST_NAME}
-                     COMMAND t_${AKTUALIZR_TEST_NAME} ${AKTUALIZR_TEST_ARGS}
-                     ${WD})
-        endif()
+
+    # run tests under valgrind if the correct CMAKE_BUILD_TYPE is set
+    if(CMAKE_BUILD_TYPE MATCHES "Valgrind")
+        add_test(NAME test_${AKTUALIZR_TEST_NAME}
+                 COMMAND ${RUN_VALGRIND} ${CMAKE_CURRENT_BINARY_DIR}/t_${AKTUALIZR_TEST_NAME} ${AKTUALIZR_TEST_ARGS}
+                 ${WD})
+    else()
+        add_test(NAME test_${AKTUALIZR_TEST_NAME}
+                 COMMAND t_${AKTUALIZR_TEST_NAME} ${AKTUALIZR_TEST_ARGS}
+                 ${WD})
+    endif()
+
     add_dependencies(build_tests t_${AKTUALIZR_TEST_NAME})
     set(TEST_SOURCES ${TEST_SOURCES} ${AKTUALIZR_TEST_SOURCES} PARENT_SCOPE)
 endfunction(add_aktualizr_test)
