0ee1b6a406f49244ee5f364af2b287b1c2c72e43
Laurent Bonnans <laurent.bonnans@here.com>
2018-07-24 17:54:10 +0200

    Add SQL db migration test from "real life" data
    From just before FSStorage was disabled

--- src/libaktualizr/storage/sqlstorage_test.cc
+++ src/libaktualizr/storage/sqlstorage_test.cc
@@ -11,7 +11,7 @@
 
 extern const std::string current_schema;
 
-boost::filesystem::path test_db_dir;
+boost::filesystem::path test_data_dir;
 
 typedef boost::tokenizer<boost::char_separator<char> > sql_tokenizer;
 
@@ -215,7 +215,7 @@ TEST(sqlstorage, migrate_root_works) {
   config.path = temp_dir.Path();
 
   boost::filesystem::remove_all(config.sqldb_path.get(config.path));
-  boost::filesystem::copy(test_db_dir / "version5.sql", config.sqldb_path.get(config.path));
+  boost::filesystem::copy(test_data_dir / "version5.sql", config.sqldb_path.get(config.path));
   SQLStorage storage(config);
 
   EXPECT_TRUE(storage.dbMigrate());
@@ -246,6 +246,47 @@ TEST(sqlstorage, migrate_root_works) {
   EXPECT_TRUE(sign["keys"].isMember("1ba3b2932863c0c6e5ff857ecdeb476b69b8b9f9ba4e36723eb10faf7768818b"));
 }
 
+TEST(sqlstorage, migrate_from_fs) {
+  TemporaryDirectory temp_dir;
+  StorageConfig config;
+  config.path = temp_dir.Path() / "config";
+
+  // copy a snapshot from a real provisioned device using fs storage
+  // (anonymized data)
+  Utils::copyDir(test_data_dir / "fs_snapshot", config.path);
+  auto storage = INvStorage::newStorage(config);
+
+  EXPECT_TRUE(storage->loadPrimaryKeys(nullptr, nullptr));
+  EXPECT_TRUE(storage->loadTlsCreds(nullptr, nullptr, nullptr));
+  EXPECT_TRUE(storage->loadLatestRoot(nullptr, Uptane::RepositoryType::Director));
+  EXPECT_TRUE(storage->loadDeviceId(nullptr));
+
+  EcuSerials serials;
+  EXPECT_TRUE(storage->loadEcuSerials(&serials));
+  EXPECT_EQ(serials.size(), 3);
+
+  std::vector<MisconfiguredEcu> misconfigured;
+  EXPECT_TRUE(storage->loadMisconfiguredEcus(&misconfigured));
+  EXPECT_EQ(misconfigured.size(), 1);
+
+  EXPECT_TRUE(storage->loadEcuRegistered());
+
+  std::vector<Uptane::Target> installed;
+  storage->loadInstalledVersions(&installed);
+  EXPECT_NE(installed.size(), 0);
+
+  EXPECT_TRUE(storage->loadInstallationResult(nullptr));
+
+  boost::filesystem::recursive_directory_iterator repo_dir_it(config.path), repo_dir_end;
+  for (; repo_dir_it != repo_dir_end; ++repo_dir_it) {
+    const auto p = repo_dir_it->path();
+    if (p == config.sqldb_path.get(config.path)) {
+      continue;
+    }
+    FAIL() << p << " should not exist anymore";
+  }
+}
+
 #ifndef __NO_MAIN__
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
@@ -256,10 +297,10 @@ int main(int argc, char** argv) {
     return 1;
   }
 
-  test_db_dir = argv[1];
+  test_data_dir = argv[1];
 
-  if (!boost::filesystem::is_directory(test_db_dir)) {
-    std::cout << test_db_dir << " is not a directory\n";
+  if (!boost::filesystem::is_directory(test_data_dir)) {
+    std::cout << test_data_dir << " is not a directory\n";
     return 1;
   }
 
