2b113513f5b02f8a1d50280a958da247af518268
Laurent Bonnans <laurent.bonnans@here.com>
2018-08-24 11:36:41 +0200

    Make aktualizr install test more verbose and helpful

--- scripts/test_install_aktualizr_and_update.sh
+++ scripts/test_install_aktualizr_and_update.sh
@@ -5,13 +5,15 @@ set -exuo pipefail
 /persistent/selfupdate_server.py 8000&
 
 dpkg-deb -I /persistent/aktualizr.deb && dpkg -i /persistent/aktualizr.deb
-aktualizr --version | grep "$(cat /persistent/aktualizr-version)"
+akt_version=$(aktualizr --version)
+(grep "$(cat /persistent/aktualizr-version)" <<< "$akt_version") || (echo "$akt_version"; false)
 
 mkdir -m 700 -p /tmp/aktualizr-storage
 aktualizr -c /persistent/selfupdate.toml --running-mode=once
 
 # check that the version was updated
-aktualizr --version | grep 2.0-selfupdate
+akt_version=$(aktualizr --version)
+(grep 2.0-selfupdate <<< "$akt_version") || (echo "$akt_version"; false)
 
 # check that aktualizr-info succeeds
 aktualizr-info
