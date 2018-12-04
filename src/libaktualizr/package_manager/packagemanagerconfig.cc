a8ed88681c7dacc42665c4c351989e21c9797421
Laurent Bonnans <laurent.bonnans@here.com>
2018-12-04 14:31:45 +0100

    Add finalizeInstall support to fake package manager
    Useful to test new OSTree installation handling
    
    Signed-off-by: Laurent Bonnans <laurent.bonnans@here.com>

--- src/libaktualizr/package_manager/packagemanagerconfig.cc
+++ src/libaktualizr/package_manager/packagemanagerconfig.cc
@@ -8,6 +8,7 @@ void PackageConfig::updateFromPropertyTree(const boost::property_tree::ptree& pt
   CopyFromConfig(sysroot, "sysroot", pt);
   CopyFromConfig(ostree_server, "ostree_server", pt);
   CopyFromConfig(packages_file, "packages_file", pt);
+  CopyFromConfig(fake_need_reboot, "fake_need_reboot", pt);
 }
 
 void PackageConfig::writeToStream(std::ostream& out_stream) const {
@@ -15,7 +16,7 @@ void PackageConfig::writeToStream(std::ostream& out_stream) const {
   writeOption(out_stream, os, "os");
   writeOption(out_stream, sysroot, "sysroot");
   writeOption(out_stream, ostree_server, "ostree_server");
-  writeOption(out_stream, packages_file, "packages_file");
+  writeOption(out_stream, fake_need_reboot, "fake_need_reboot");
 }
 
 std::ostream& operator<<(std::ostream& os, PackageManager pm) {
