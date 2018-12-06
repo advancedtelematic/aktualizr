advancedtelematic/aktualizr

commit 79283a3c77a584d9a7f5e48f83297c4201e394fe

Async api with std::future
Signed-off-by: Serhiy Stetskovych <patriotyk@gmail.com>

src/aktualizr_primary/main.cc
@@ -140,7 +140,7 @@ int main(int argc, char *argv[]) {
         break;
       case RunningMode::kFull:
       default:
-        aktualizr.RunForever();
+        aktualizr.RunForever().get();
         break;
     }
     r = 0;

src/hmi_stub/main.cc
@@ -149,11 +149,11 @@ int main(int argc, char *argv[]) {
       } else if (buffer == "senddevicedata") {
         aktualizr.SendDeviceData();
       } else if (buffer == "fetchmetadata" || buffer == "fetchmeta" || buffer == "checkupdates" || buffer == "check") {
-        UpdateCheckResult result = aktualizr.CheckUpdates();
+        UpdateCheckResult result = aktualizr.CheckUpdates().get();
         updates = result.updates;
         std::cout << updates.size() << " updates available\n";
       } else if (buffer == "download" || buffer == "startdownload") {
-        std::thread([&aktualizr, updates] { aktualizr.Download(updates); }).detach();
+        aktualizr.Download(updates);
       } else if (buffer == "install" || buffer == "uptaneinstall") {
         aktualizr.Install(updates);
         updates.clear();

src/libaktualizr/primary/aktualizr.cc
@@ -43,58 +43,110 @@ void Aktualizr::systemSetup() {
   LOG_TRACE << "... seeding complete in " << timer;
 }
 
-void Aktualizr::Initialize() { uptane_client_->initialize(); }
+void Aktualizr::Initialize() {
+  uptane_client_->initialize();
+  api_thread_ = std::thread([&]() {
+    while (!shutdown_) {
+      auto task = api_queue_.dequeue();
+      if (shutdown_) {
+        return;
+      }
+      task();
+    }
+  });
+}
 
 void Aktualizr::UptaneCycle() {
   RunningMode running_mode = config_.uptane.running_mode;
-  UpdateCheckResult update_result = CheckUpdates();
+  UpdateCheckResult update_result = CheckUpdates().get();
   if (running_mode == RunningMode::kCheck || update_result.updates.size() == 0) {
     return;
   } else if (running_mode == RunningMode::kInstall) {
-    Install(update_result.updates);
+    uptane_client_->uptaneInstall(update_result.updates);
     uptane_client_->putManifest();
     return;
   }
 
-  DownloadResult download_result = Download(update_result.updates);
+  DownloadResult download_result = Download(update_result.updates).get();
   if (running_mode == RunningMode::kDownload || download_result.status != DownloadStatus::kSuccess ||
       download_result.updates.size() == 0) {
     return;
   }
 
-  Install(download_result.updates);
+  uptane_client_->uptaneInstall(download_result.updates);
   uptane_client_->putManifest();
 }
 
-int Aktualizr::RunForever() {
-  SendDeviceData();
-  while (!shutdown_) {
-    UptaneCycle();
-    std::this_thread::sleep_for(std::chrono::seconds(config_.uptane.polling_sec));
-  }
-  return EXIT_SUCCESS;
+std::future<void> Aktualizr::RunForever() {
+  std::future<void> future = std::async(std::launch::async, [&]() {
+    SendDeviceData().get();
+    while (!shutdown_) {
+      UptaneCycle();
+      std::this_thread::sleep_for(std::chrono::seconds(config_.uptane.polling_sec));
+    }
+  });
+  return future;
 }
 
 void Aktualizr::AddSecondary(const std::shared_ptr<Uptane::SecondaryInterface> &secondary) {
   uptane_client_->addNewSecondary(secondary);
 }
 
-void Aktualizr::Shutdown() { shutdown_ = true; }
-
-CampaignCheckResult Aktualizr::CampaignCheck() { return uptane_client_->campaignCheck(); }
-
-void Aktualizr::CampaignAccept(const std::string &campaign_id) { uptane_client_->campaignAccept(campaign_id); }
-
-void Aktualizr::SendDeviceData() { uptane_client_->sendDeviceData(); }
-
-UpdateCheckResult Aktualizr::CheckUpdates() { return uptane_client_->fetchMeta(); }
-
-DownloadResult Aktualizr::Download(const std::vector<Uptane::Target> &updates) {
-  return uptane_client_->downloadImages(updates);
+void Aktualizr::Shutdown() {
+  if (!shutdown_) {
+    shutdown_ = true;
+    api_queue_.shutDown();
+  }
 }
 
-InstallResult Aktualizr::Install(const std::vector<Uptane::Target> &updates) {
-  return uptane_client_->uptaneInstall(updates);
+std::future<CampaignCheckResult> Aktualizr::CampaignCheck() {
+  auto promise = std::make_shared<std::promise<CampaignCheckResult>>();
+  std::function<void()> task([&, promise]() { promise->set_value(uptane_client_->campaignCheck()); });
+  api_queue_.enqueue(task);
+  return promise->get_future();
+}
+
+std::future<void> Aktualizr::CampaignAccept(const std::string &campaign_id) {
+  auto promise = std::make_shared<std::promise<void>>();
+  std::function<void()> task([&, promise, campaign_id]() {
+    uptane_client_->campaignAccept(campaign_id);
+    promise->set_value();
+  });
+  api_queue_.enqueue(task);
+  return promise->get_future();
+}
+
+std::future<void> Aktualizr::SendDeviceData() {
+  auto promise = std::make_shared<std::promise<void>>();
+  std::function<void()> task([&, promise]() {
+    uptane_client_->sendDeviceData();
+    promise->set_value();
+  });
+  api_queue_.enqueue(task);
+  return promise->get_future();
+}
+
+std::future<UpdateCheckResult> Aktualizr::CheckUpdates() {
+  auto promise = std::make_shared<std::promise<UpdateCheckResult>>();
+  std::function<void()> task([&, promise]() { promise->set_value(uptane_client_->fetchMeta()); });
+  api_queue_.enqueue(task);
+  return promise->get_future();
+}
+
+std::future<DownloadResult> Aktualizr::Download(const std::vector<Uptane::Target> &updates) {
+  auto promise = std::make_shared<std::promise<DownloadResult>>();
+  std::function<void()> task(
+      [this, promise, updates]() { promise->set_value(uptane_client_->downloadImages(updates)); });
+  api_queue_.enqueue(task);
+  return promise->get_future();
+}
+
+std::future<InstallResult> Aktualizr::Install(const std::vector<Uptane::Target> &updates) {
+  auto promise = std::make_shared<std::promise<InstallResult>>();
+  std::function<void()> task(
+      [this, promise, updates]() { promise->set_value(uptane_client_->uptaneInstall(updates)); });
+  api_queue_.enqueue(task);
+  return promise->get_future();
 }
 
 PauseResult Aktualizr::Pause() { return uptane_client_->pause(); }

src/libaktualizr/primary/aktualizr.h
@@ -13,6 +13,38 @@
 #include "uptane/secondaryinterface.h"
 #include "utilities/events.h"
 
+class ApiQueue {
+ public:
+  void enqueue(const std::function<void()>& t) {
+    std::lock_guard<std::mutex> lock(m_);
+    q_.push(t);
+    c_.notify_one();
+  }
+
+  std::function<void()> dequeue() {
+    std::unique_lock<std::mutex> wait_lock(m_);
+    while (q_.empty()) {
+      c_.wait(wait_lock);
+      if (shutdown_) {
+        return std::function<void()>();
+      }
+    }
+    std::function<void()> val = q_.front();
+    q_.pop();
+    return val;
+  }
+  void shutDown() {
+    shutdown_ = true;
+    enqueue(std::function<void()>());
+  }
+
+ private:
+  std::queue<std::function<void()>> q_;
+  mutable std::mutex m_;
+  std::condition_variable c_;
+  std::atomic_bool shutdown_{false};
+};
+
 /**
  * This class provides the main APIs necessary for launching and controlling
  * libaktualizr.
@@ -22,6 +54,12 @@ class Aktualizr {
   /** Aktualizr requires a configuration object. Examples can be found in the
    *  config directory. */
   explicit Aktualizr(Config& config);
+  ~Aktualizr() {
+    Shutdown();
+    if (api_thread_.joinable()) {
+      api_thread_.join();
+    }
+  }
   Aktualizr(const Aktualizr&) = delete;
   Aktualizr& operator=(const Aktualizr&) = delete;
 
@@ -33,11 +71,11 @@ class Aktualizr {
   void Initialize();
 
   /**
-   * Run aktualizr indefinitely until Shutdown is called. Intended to be used
-   * with the Full \ref RunningMode setting. You may want to run this on its own
-   * thread.
+   * Asynchronously run aktualizr indefinitely until Shutdown is called.
+   * Intended to be used with the Full \ref RunningMode setting.
+   * @return Empty std::future object
    */
-  int RunForever();
+  std::future<void> RunForever();
 
   /**
    * Asynchronously shut aktualizr down if it is running indefinitely with the
@@ -49,23 +87,25 @@ class Aktualizr {
    * Check for campaigns.
    * Campaigns are a concept outside of Uptane, and allow for user approval of
    * updates before the contents of the update are known.
-   * @return Data about available campaigns.
+   * @return std::future object with data about available campaigns.
    */
-  CampaignCheckResult CampaignCheck();
+  std::future<CampaignCheckResult> CampaignCheck();
 
   /**
    * Accept a campaign for the current device.
    * Campaigns are a concept outside of Uptane, and allow for user approval of
    * updates before the contents of the update are known.
    * @param campaign_id Campaign ID as provided by CampaignCheck.
+   * @return Empty std::future object
    */
-  void CampaignAccept(const std::string& campaign_id);
+  std::future<void> CampaignAccept(const std::string& campaign_id);
 
   /**
    * Send local device data to the server.
    * This includes network status, installed packages, hardware etc.
+   * @return Empty std::future object
    */
-  void SendDeviceData();
+  std::future<void> SendDeviceData();
 
   /**
    * Fetch Uptane metadata and check for updates.
@@ -74,14 +114,14 @@ class Aktualizr {
    * for target updates.
    * @return Information about available updates.
    */
-  UpdateCheckResult CheckUpdates();
+  std::future<UpdateCheckResult> CheckUpdates();
 
   /**
    * Download targets.
    * @param updates Vector of targets to download as provided by CheckUpdates.
-   * @return Information about download results.
+   * @return std::future object with information about download results.
    */
-  DownloadResult Download(const std::vector<Uptane::Target>& updates);
+  std::future<DownloadResult> Download(const std::vector<Uptane::Target>& updates);
 
   /**
    * Get target downloaded in Download call. Returned target is guaranteed to be verified and up-to-date
@@ -95,9 +135,9 @@ class Aktualizr {
    * Install targets.
    * @param updates Vector of targets to install as provided by CheckUpdates or
    * Download.
-   * @return Information about installation results.
+   * @return std::future object with information about installation results.
    */
-  InstallResult Install(const std::vector<Uptane::Target>& updates);
+  std::future<InstallResult> Install(const std::vector<Uptane::Target>& updates);
 
   /**
    * Pause a download current in progress.
@@ -141,6 +181,7 @@ class Aktualizr {
   FRIEND_TEST(Aktualizr, InstallWithUpdates);
   FRIEND_TEST(Aktualizr, CampaignCheck);
   FRIEND_TEST(Aktualizr, FullNoCorrelationId);
+  FRIEND_TEST(Aktualizr, APICheck);
   Aktualizr(Config& config, std::shared_ptr<INvStorage> storage_in, std::shared_ptr<SotaUptaneClient> uptane_client_in,
             std::shared_ptr<event::Channel> sig_in);
   void systemSetup();
@@ -150,6 +191,8 @@ class Aktualizr {
   std::shared_ptr<SotaUptaneClient> uptane_client_;
   std::shared_ptr<event::Channel> sig_;
   std::atomic<bool> shutdown_ = {false};
+  ApiQueue api_queue_;
+  std::thread api_thread_;
 };
 
 #endif  // AKTUALIZR_H_

src/libaktualizr/primary/aktualizr_test.cc
@@ -97,13 +97,13 @@ TEST(Aktualizr, FullNoUpdates) {
   boost::signals2::connection conn = aktualizr.SetSignalHandler(f_cb);
 
   aktualizr.Initialize();
-  UpdateCheckResult result = aktualizr.CheckUpdates();
+  UpdateCheckResult result = aktualizr.CheckUpdates().get();
   EXPECT_EQ(result.ecus_count, 0);
   EXPECT_EQ(result.updates.size(), 0);
   EXPECT_EQ(result.status, UpdateStatus::kNoUpdatesAvailable);
   // Fetch twice so that we can check for a second UpdateCheckComplete and
   // guarantee that nothing unexpected happened after the first fetch.
-  result = aktualizr.CheckUpdates();
+  result = aktualizr.CheckUpdates().get();
   EXPECT_EQ(result.ecus_count, 0);
   EXPECT_EQ(result.updates.size(), 0);
   EXPECT_EQ(result.status, UpdateStatus::kNoUpdatesAvailable);
@@ -149,7 +149,6 @@ int num_events_FullWithUpdates = 0;
 std::future<void> future_FullWithUpdates{};
 std::promise<void> promise_FullWithUpdates{};
 void process_events_FullWithUpdates(const std::shared_ptr<event::BaseEvent>& event) {
-  std::cout << "Got " << event->variant << "\n";
   if (event->variant == "DownloadProgressReport") {
     return;
   }
@@ -261,7 +260,6 @@ TEST(Aktualizr, FullWithUpdates) {
 
   aktualizr.Initialize();
   aktualizr.UptaneCycle();
-
   std::future_status status = future_FullWithUpdates.wait_for(std::chrono::seconds(20));
   if (status != std::future_status::ready) {
     FAIL() << "Timed out waiting for installation to complete.";
@@ -390,7 +388,6 @@ TEST(Aktualizr, CheckWithUpdates) {
 
   aktualizr.Initialize();
   aktualizr.UptaneCycle();
-
   std::future_status status = future_CheckWithUpdates.wait_for(std::chrono::seconds(20));
   if (status != std::future_status::ready) {
     FAIL() << "Timed out waiting for metadata to be fetched.";
@@ -476,7 +473,7 @@ TEST(Aktualizr, DownloadWithUpdates) {
 
   aktualizr.Initialize();
   // First try downloading nothing. Nothing should happen.
-  DownloadResult result = aktualizr.Download(std::vector<Uptane::Target>());
+  DownloadResult result = aktualizr.Download(std::vector<Uptane::Target>()).get();
   EXPECT_EQ(result.updates.size(), 0);
   EXPECT_EQ(result.status, DownloadStatus::kNothingToDownload);
   aktualizr.UptaneCycle();
@@ -630,7 +627,7 @@ TEST(Aktualizr, InstallWithUpdates) {
   Uptane::Target secondary_target("secondary_firmware.txt", secondary_json);
 
   // First try installing nothing. Nothing should happen.
-  InstallResult result = aktualizr.Install(updates_InstallWithUpdates);
+  InstallResult result = aktualizr.Install(updates_InstallWithUpdates).get();
   EXPECT_EQ(result.reports.size(), 0);
 
   EXPECT_EQ(aktualizr.GetStoredTarget(primary_target).get(), nullptr)
@@ -680,7 +677,7 @@ TEST(Aktualizr, CampaignCheck) {
   Aktualizr aktualizr(conf, storage, up, sig);
 
   aktualizr.Initialize();
-  auto result = aktualizr.CampaignCheck();
+  auto result = aktualizr.CampaignCheck().get();
   EXPECT_EQ(result.campaigns.size(), 1);
 }
 
@@ -716,19 +713,66 @@ TEST(Aktualizr, FullNoCorrelationId) {
     Aktualizr aktualizr(conf, storage, up, sig);
 
     aktualizr.Initialize();
-    UpdateCheckResult update_result = aktualizr.CheckUpdates();
+    UpdateCheckResult update_result = aktualizr.CheckUpdates().get();
     EXPECT_EQ(update_result.status, UpdateStatus::kUpdatesAvailable);
 
-    DownloadResult download_result = aktualizr.Download(update_result.updates);
+    DownloadResult download_result = aktualizr.Download(update_result.updates).get();
     EXPECT_EQ(download_result.status, DownloadStatus::kSuccess);
 
-    InstallResult install_result = aktualizr.Install(download_result.updates);
+    InstallResult install_result = aktualizr.Install(download_result.updates).get();
     for (const auto& r : install_result.reports) {
       EXPECT_EQ(r.status.result_code, data::UpdateResultCode::kOk);
     }
   }
 
   EXPECT_EQ(http->events_seen, 8);
+}
+
+int num_events_UpdateCheck = 0;
+void process_events_UpdateCheck(const std::shared_ptr<event::BaseEvent>& event) {
+  std::cout << event->variant << "\n";
+  if (event->variant == "UpdateCheckComplete") {
+    num_events_UpdateCheck++;
+  }
+}
+
+TEST(Aktualizr, APICheck) {
+  TemporaryDirectory temp_dir;
+  auto http = std::make_shared<HttpFake>(temp_dir.Path(), "hasupdates");
+
+  Config conf = makeTestConfig(temp_dir, http->tls_server);
+
+  auto storage = INvStorage::newStorage(conf.storage);
+  auto sig = std::make_shared<boost::signals2::signal<void(std::shared_ptr<event::BaseEvent>)>>();
+  auto up = SotaUptaneClient::newTestClient(conf, storage, http, sig);
+  std::function<void(std::shared_ptr<event::BaseEvent> event)> f_cb = process_events_UpdateCheck;
+
+  {
+    Aktualizr aktualizr(conf, storage, up, sig);
+    boost::signals2::connection conn = aktualizr.SetSignalHandler(f_cb);
+    aktualizr.Initialize();
+    for (int i = 0; i < 5; ++i) {
+      aktualizr.CheckUpdates();
+    }
+    std::this_thread::sleep_for(std::chrono::seconds(12));
+    aktualizr.Shutdown();
+  }
+
+  EXPECT_EQ(num_events_UpdateCheck, 5);
+
+  num_events_UpdateCheck = 0;
+  // try again, but shutdown before it finished all calls
+  {
+    Aktualizr aktualizr(conf, storage, up, sig);
+    boost::signals2::connection conn = aktualizr.SetSignalHandler(f_cb);
+    aktualizr.Initialize();
+    for (int i = 0; i < 100; ++i) {
+      aktualizr.CheckUpdates();
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(800));
+    aktualizr.Shutdown();
+  }
+  EXPECT_LT(num_events_UpdateCheck, 100);
 }
 
 #ifndef __NO_MAIN__

commit 3c4aae08b4850ae3e263c21f4fa0026bbae17216

Fix races in aktualizr with running modes
We did not wait for the result of the futures after issuing commands
like "CheckUpdates".

This broke the existing behaviour of `aktualizr check`.

Bug introduced in 79283a3c77a584d9a7f5e48f83297c4201e394fe

Signed-off-by: Laurent Bonnans <laurent.bonnans@here.com>

src/aktualizr_primary/main.cc
@@ -125,17 +125,17 @@ int main(int argc, char *argv[]) {
     // launch the first event
     switch (running_mode) {
       case RunningMode::kCampaignCheck:
-        aktualizr.CampaignCheck();
+        aktualizr.CampaignCheck().get();
         break;
       case RunningMode::kCampaignAccept:
         if (commandline_map.count("campaign-id") == 0) {
           throw std::runtime_error("Running mode " + StringFromRunningMode(running_mode) + " requires a campaign id");
         }
-        aktualizr.CampaignAccept(commandline_map["campaign-id"].as<std::string>());
+        aktualizr.CampaignAccept(commandline_map["campaign-id"].as<std::string>()).get();
         break;
       case RunningMode::kCheck:
-        aktualizr.SendDeviceData();
-        aktualizr.CheckUpdates();
+        aktualizr.SendDeviceData().get();
+        aktualizr.CheckUpdates().get();
         break;
       case RunningMode::kDownload:
       case RunningMode::kInstall: