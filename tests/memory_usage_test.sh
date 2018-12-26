78cd3af74a32fc70fc345f5f1287594d35aa8f23
Serhiy Stetskovych <patriotyk@gmail.com>
2018-12-26 17:29:09 +0200

    Use debian package manager in memory test
    Signed-off-by: Serhiy Stetskovych <patriotyk@gmail.com>

--- tests/memory_usage_test.sh
+++ tests/memory_usage_test.sh
@@ -9,14 +9,31 @@ PATH=$PATH:"$1/src/aktualizr_repo/"
 ./src/aktualizr_repo/run/create_repo.sh $TEMP_DIR localhost
 
 TARGET_SIZE="200M"
-fallocate -l $TARGET_SIZE target1
 
-$1/src/aktualizr_repo/aktualizr-repo image --filename target1 --path $TEMP_DIR/uptane
-$1/src/aktualizr_repo/aktualizr-repo addtarget --filename target1 --path $TEMP_DIR/uptane --hwid desktop --serial serial1
+mkdir -p $TEMP_DIR/deb/DEBIAN
+mkdir -p $TEMP_DIR/deb/usr/share
+fallocate -l $TARGET_SIZE $TEMP_DIR/deb/usr/share/target1
+
+cat > $TEMP_DIR/deb/DEBIAN/control <<EOF
+Package: testdeb
+Version: 1.1-1
+Section: base
+Priority: optional
+Architecture: all
+Maintainer: Serhiy Stetskovych <test@mal.com>
+Description: Some fake package
+ This is fake package.
+EOF
+
+dpkg-deb -Znone -b $TEMP_DIR/deb $TEMP_DIR/good.deb
+PATH="tests/test_data/fake_dpkg":$PATH
+$1/src/aktualizr_repo/aktualizr-repo image --filename $TEMP_DIR/good.deb --path $TEMP_DIR/uptane
+$1/src/aktualizr_repo/aktualizr-repo addtarget --filename good.deb --path $TEMP_DIR/uptane --hwid desktop --serial serial1
 $1/src/aktualizr_repo/aktualizr-repo signtargets  --path $TEMP_DIR/uptane
 
 
 sed -i 's/\[provision\]/\[provision\]\nprimary_ecu_serial = serial1/g'  "$TEMP_DIR/sota.toml"
+sed -i 's/type = "none"/type = "debian"/g' "$TEMP_DIR/sota.toml"
 
 ./src/aktualizr_repo/run/serve_repo.py 9000 "$TEMP_DIR" &
 
