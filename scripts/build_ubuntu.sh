5e7d31d2ab91bd650564895ee9a304c087b63c8f
Manikandan Kandasamy <Manikandan.Kandasamy@here.com>
2019-01-09 11:45:05 +0100

    OTA-1937: Add support for gitlab
    Signed-off-by: Manikandan Kandasamy <Manikandan.Kandasamy@here.com>

--- scripts/build_ubuntu.sh
+++ scripts/build_ubuntu.sh
@@ -13,6 +13,7 @@ export TEST_WITH_TESTSUITE=0
 
 # build and copy aktualizr.deb and garage_deploy.deb to $TEST_INSTALL_DESTDIR
 mkdir -p "$TEST_INSTALL_DESTDIR"
+git submodule update --init --recursive
 "$GITREPO_ROOT/scripts/test.sh"
 
 # copy provisioning data and scripts
