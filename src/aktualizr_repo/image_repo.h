c343b31e702de02ea43193e181f26f9f37835f99
Serhiy Stetskovych <patriotyk@gmail.com>
2019-01-23 10:33:38 +0200

    Use --targetname to override --imagepath for target name
    Signed-off-by: Serhiy Stetskovych <patriotyk@gmail.com>

--- src/aktualizr_repo/image_repo.h
+++ src/aktualizr_repo/image_repo.h
@@ -7,7 +7,8 @@ class ImageRepo : public Repo {
  public:
   ImageRepo(boost::filesystem::path path, const std::string &expires, std::string correlation_id)
       : Repo(Uptane::RepositoryType::Image(), std::move(path), expires, std::move(correlation_id)) {}
-  void addImage(const boost::filesystem::path &image_path, const Delegation &delegation = {});
+  void addImage(const boost::filesystem::path &image_path, const boost::filesystem::path &targetname,
+                const Delegation &delegation = {});
   void addCustomImage(const std::string &name, const Uptane::Hash &hash, uint64_t length, const Delegation &delegation);
   void addDelegation(const Uptane::Role &name, const std::string &path, KeyType key_type = KeyType::kRSA2048,
                      bool terminating = false);
