0fc1526f6f835e90e71cec083921aacdff1ac418
Patrick Vacek <patrickvacek@gmail.com>
2019-02-12 16:14:35 +0100

    Add "/delegations" to URLs of delegations to match backend.
    Signed-off-by: Patrick Vacek <patrickvacek@gmail.com>

--- src/libaktualizr/uptane/fetcher.cc
+++ src/libaktualizr/uptane/fetcher.cc
@@ -9,9 +9,11 @@ namespace Uptane {
 bool Fetcher::fetchRole(std::string* result, int64_t maxsize, RepositoryType repo, const Uptane::Role& role,
                         Version version) {
   // TODO: chain-loading root.json
-  std::string base_url =
-      (repo == RepositoryType::Director()) ? config.uptane.director_server : config.uptane.repo_server;
-  std::string url = base_url + "/" + version.RoleFileName(role);
+  std::string url = (repo == RepositoryType::Director()) ? config.uptane.director_server : config.uptane.repo_server;
+  if (role.IsDelegation()) {
+    url += "/delegations";
+  }
+  url += "/" + version.RoleFileName(role);
   HttpResponse response = http->get(url, maxsize);
   if (!response.isOk()) {
     return false;
