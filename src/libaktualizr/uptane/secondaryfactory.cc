cc7242dfca9467d23a7956345adc80f24fcaf92a
Laurent Bonnans <laurent.bonnans@here.com>
2019-03-12 16:22:18 +0100

    Make aktualizr buildable without partial and isotp checkout
    Signed-off-by: Laurent Bonnans <laurent.bonnans@here.com>

---
+++ src/libaktualizr/uptane/secondaryfactory.cc
@@ -0,0 +1,46 @@
+#include "uptane/secondaryfactory.h"
+
+#include "logging/logging.h"
+#include "uptane/ipuptanesecondary.h"
+#include "uptane/virtualsecondary.h"
+
+#ifdef ISOTP_SECONDARY_ENABLED
+#include "isotpsecondary.h"
+#endif
+
+#ifdef OPCUA_SECONDARY_ENABLED
+#include "uptane/opcuasecondary.h"
+#endif
+
+namespace Uptane {
+
+std::shared_ptr<SecondaryInterface> SecondaryFactory::makeSecondary(const SecondaryConfig& sconfig) {
+  switch (sconfig.secondary_type) {
+    case SecondaryType::kVirtual:
+      return std::make_shared<VirtualSecondary>(sconfig);
+    case SecondaryType::kLegacy:
+      LOG_ERROR << "Legacy secondary support is deprecated.";
+      return std::shared_ptr<SecondaryInterface>();  // NULL-equivalent
+    case SecondaryType::kIpUptane:
+      return std::make_shared<IpUptaneSecondary>(sconfig);
+    case SecondaryType::kIsoTpUptane:
+#ifdef ISOTP_SECONDARY_ENABLED
+      return std::make_shared<IsoTpSecondary>(sconfig);
+#else
+      LOG_ERROR << "libaktualizr was built without ISO/TP secondary support.";
+      return std::shared_ptr<SecondaryInterface>();  // NULL-equivalent
+#endif
+    case SecondaryType::kOpcuaUptane:
+#ifdef OPCUA_SECONDARY_ENABLED
+      return std::make_shared<OpcuaSecondary>(sconfig);
+#else
+      LOG_ERROR << "libaktualizr was built without OPC-UA secondary support.";
+      return std::shared_ptr<SecondaryInterface>();  // NULL-equivalent
+#endif
+    default:
+      LOG_ERROR << "Unrecognized secondary type: " << static_cast<int>(sconfig.secondary_type);
+      return std::shared_ptr<SecondaryInterface>();  // NULL-equivalent
+  }
+}
+
+}  // namespace Uptane
--- src/libaktualizr/uptane/secondaryfactory.h
+++ src/libaktualizr/uptane/secondaryfactory.h
@@ -1,47 +1,15 @@
 #ifndef UPTANE_SECONDARYFACTORY_H_
 #define UPTANE_SECONDARYFACTORY_H_
 
-#include "ipuptanesecondary.h"
-#include "isotpsecondary.h"
-#include "logging/logging.h"
-#include "uptane/ipuptanesecondary.h"
-#include "uptane/opcuasecondary.h"
+#include <memory>
 #include "uptane/secondaryconfig.h"
 #include "uptane/secondaryinterface.h"
-#include "uptane/virtualsecondary.h"
 
 namespace Uptane {
 
 class SecondaryFactory {
  public:
-  static std::shared_ptr<SecondaryInterface> makeSecondary(const SecondaryConfig& sconfig) {
-    switch (sconfig.secondary_type) {
-      case SecondaryType::kVirtual:
-        return std::make_shared<VirtualSecondary>(sconfig);
-      case SecondaryType::kLegacy:
-        LOG_ERROR << "Legacy secondary support is deprecated.";
-        return std::shared_ptr<SecondaryInterface>();  // NULL-equivalent
-      case SecondaryType::kIpUptane:
-        return std::make_shared<IpUptaneSecondary>(sconfig);
-      case SecondaryType::kIsoTpUptane:
-#ifdef ISOTP_SECONDARY_ENABLED
-        return std::make_shared<IsoTpSecondary>(sconfig);
-#else
-        LOG_ERROR << "libaktualizr was built without ISO/TP secondary support.";
-        return std::shared_ptr<SecondaryInterface>();  // NULL-equivalent
-#endif
-      case SecondaryType::kOpcuaUptane:
-#ifdef OPCUA_SECONDARY_ENABLED
-        return std::make_shared<OpcuaSecondary>(sconfig);
-#else
-        LOG_ERROR << "libaktualizr was built without OPC-UA secondary support.";
-        return std::shared_ptr<SecondaryInterface>();  // NULL-equivalent
-#endif
-      default:
-        LOG_ERROR << "Unrecognized secondary type: " << static_cast<int>(sconfig.secondary_type);
-        return std::shared_ptr<SecondaryInterface>();  // NULL-equivalent
-    }
-  }
+  static std::shared_ptr<SecondaryInterface> makeSecondary(const SecondaryConfig& sconfig);
 };
 }  // namespace Uptane
 
