6dc6928e98be54c181372b387ce5750979822b94
Patrick Vacek <patrickvacek@gmail.com>
2019-04-18 14:03:11 +0200

    Add test to verify that we handle empty targets metadata correctly.
    Signed-off-by: Patrick Vacek <patrickvacek@gmail.com>

---
+++ src/libaktualizr/primary/empty_targets_test.cc
@@ -0,0 +1,115 @@
+#include <gtest/gtest.h>
+
+#include <string>
+
+#include <boost/process.hpp>
+
+#include "httpfake.h"
+#include "primary/aktualizr.h"
+#include "test_utils.h"
+#include "uptane_test_common.h"
+
+boost::filesystem::path aktualizr_repo_path;
+
+class HttpRejectEmptyCorrId : public HttpFake {
+ public:
+  HttpRejectEmptyCorrId(const boost::filesystem::path &test_dir_in, const boost::filesystem::path &meta_dir_in)
+      : HttpFake(test_dir_in, "", meta_dir_in) {}
+
+  HttpResponse put(const std::string &url, const Json::Value &data) override {
+    last_manifest = data;
+    if (url.find("/manifest") != std::string::npos) {
+      if (data["signed"].isMember("installation_report") &&
+          data["signed"]["installation_report"]["report"]["correlation_id"].asString().empty()) {
+        EXPECT_FALSE(data["signed"]["installation_report"]["report"]["correlation_id"].asString().empty());
+        return HttpResponse(url, 400, CURLE_OK, "");
+      }
+    }
+    return HttpResponse(url, 200, CURLE_OK, "");
+  }
+};
+
+/*
+ * Verify that we can successfully install an update after receiving
+ * subsequent targets metadata that is empty.
+ */
+TEST(Aktualizr, EmptyTargets) {
+  TemporaryDirectory temp_dir;
+  TemporaryDirectory meta_dir;
+  auto http = std::make_shared<HttpRejectEmptyCorrId>(temp_dir.Path(), meta_dir.Path() / "repo");
+  Config conf = UptaneTestCommon::makeTestConfig(temp_dir, http->tls_server);
+  conf.pacman.fake_need_reboot = true;
+  conf.bootloader.reboot_sentinel_dir = temp_dir / "aktualizr-session";
+  logger_set_threshold(boost::log::trivial::trace);
+
+  Process akt_repo(aktualizr_repo_path.string());
+  akt_repo.run({"generate", "--path", meta_dir.PathString(), "--correlationid", "abc123"});
+  akt_repo.run({"image", "--path", meta_dir.PathString(), "--filename", "tests/test_data/firmware.txt", "--targetname",
+                "firmware.txt"});
+  akt_repo.run({"addtarget", "--path", meta_dir.PathString(), "--targetname", "firmware.txt", "--hwid", "primary_hw",
+                "--serial", "CA:FE:A6:D2:84:9D"});
+  akt_repo.run({"signtargets", "--path", meta_dir.PathString(), "--correlationid", "abc123"});
+  // Work around inconsistent directory naming.
+  Utils::copyDir(meta_dir.Path() / "repo/image", meta_dir.Path() / "repo/repo");
+
+  auto storage = INvStorage::newStorage(conf.storage);
+  {
+    Aktualizr aktualizr(conf, storage, http);
+    aktualizr.Initialize();
+
+    result::UpdateCheck update_result = aktualizr.CheckUpdates().get();
+    EXPECT_EQ(update_result.status, result::UpdateStatus::kUpdatesAvailable);
+
+    result::Download download_result = aktualizr.Download(update_result.updates).get();
+    EXPECT_EQ(download_result.status, result::DownloadStatus::kSuccess);
+
+    akt_repo.run({"emptytargets", "--path", meta_dir.PathString()});
+    akt_repo.run({"signtargets", "--path", meta_dir.PathString(), "--correlationid", "abc123"});
+
+    result::UpdateCheck update_result2 = aktualizr.CheckUpdates().get();
+    EXPECT_EQ(update_result2.status, result::UpdateStatus::kUpdatesAvailable);
+
+    result::Install install_result = aktualizr.Install(update_result2.updates).get();
+    EXPECT_EQ(install_result.ecu_reports.size(), 1);
+    EXPECT_EQ(install_result.ecu_reports[0].install_res.result_code.num_code,
+              data::ResultCode::Numeric::kNeedCompletion);
+
+    aktualizr.uptane_client_->package_manager_->completeInstall();
+  }
+  {
+    Aktualizr aktualizr(conf, storage, http);
+    aktualizr.Initialize();
+
+    const Json::Value manifest = http->last_manifest["signed"];
+    const Json::Value manifest_versions = manifest["ecu_version_manifests"];
+
+    EXPECT_EQ(manifest["installation_report"]["report"]["items"].size(), 1);
+    EXPECT_EQ(manifest["installation_report"]["report"]["items"][0]["ecu"].asString(), "CA:FE:A6:D2:84:9D");
+    EXPECT_TRUE(manifest["installation_report"]["report"]["items"][0]["result"]["success"].asBool());
+
+    EXPECT_EQ(manifest_versions["CA:FE:A6:D2:84:9D"]["signed"]["installed_image"]["filepath"].asString(),
+              "firmware.txt");
+    EXPECT_EQ(manifest_versions["CA:FE:A6:D2:84:9D"]["signed"]["installed_image"]["fileinfo"]["length"].asUInt(), 17);
+
+    result::UpdateCheck update_result3 = aktualizr.CheckUpdates().get();
+    EXPECT_EQ(update_result3.status, result::UpdateStatus::kNoUpdatesAvailable);
+  }
+}
+
+#ifndef __NO_MAIN__
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  if (argc != 2) {
+    std::cerr << "Error: " << argv[0] << " requires the path to the aktualizr-repo utility\n";
+    return EXIT_FAILURE;
+  }
+  aktualizr_repo_path = argv[1];
+
+  logger_init();
+  logger_set_threshold(boost::log::trivial::trace);
+
+  return RUN_ALL_TESTS();
+}
+#endif
+
+// vim: set tabstop=2 shiftwidth=2 expandtab:
