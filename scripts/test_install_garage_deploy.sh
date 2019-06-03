cfd9d7bcdc353d32946c5b055085364b176d3863
Laurent Bonnans <laurent.bonnans@here.com>
2019-06-03 16:19:33 +0200

    Split build and test of deb pkgs in CI
    Signed-off-by: Laurent Bonnans <laurent.bonnans@here.com>

--- scripts/test_install_garage_deploy.sh
+++ scripts/test_install_garage_deploy.sh
@@ -2,6 +2,8 @@
 
 set -exuo pipefail
 
-dpkg -i /persistent/garage_deploy*.deb
+TEST_INSTALL_DESTDIR=${TEST_INSTALL_DESTDIR:-/persistent}
+
+dpkg -i "$TEST_INSTALL_DESTDIR"/garage_deploy*.deb
 garage-deploy --version
 garage-sign --help
