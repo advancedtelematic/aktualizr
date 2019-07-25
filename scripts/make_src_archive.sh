59f4868e8581a5e90aab5f0c4f13be2e005cf12b
Laurent Bonnans <laurent.bonnans@here.com>
2019-07-25 16:39:49 +0200

    Move GitHub release generation to gitlab
    Signed-off-by: Laurent Bonnans <laurent.bonnans@here.com>

--- scripts/make_src_archive.sh
+++ scripts/make_src_archive.sh
@@ -2,10 +2,14 @@
 
 set -eu
 
-REPO=$1
-OUTPUT=$2
+OUTPUT=$1
+REPO=${2:-.}
 
-pip3 install --user "https://files.pythonhosted.org/packages/60/44/26791beafa5c2c1f8ac0dd38a7761a0162c0f31bdad91a0091a48a923a44/git_archive_all-1.19.4-py2.py3-none-any.whl"
+python3 -m venv venv
+
+. venv/bin/activate
+
+pip install 'git_archive_all==1.19.4'
 
 cd "$REPO"
 git-archive-all "$OUTPUT"
