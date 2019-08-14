88c2c377f541ae020cc08d103f96b4abacbd1725
Andy Doan <andy@foundries.io>
2019-08-14 09:45:29 -0500

    docker-app: Fix clang-tidy warnings
    Signed-off-by: Andy Doan <andy@foundries.io>

--- src/libaktualizr/package_manager/dockerappmanager.h
+++ src/libaktualizr/package_manager/dockerappmanager.h
@@ -10,16 +10,16 @@ class DockerAppManager : public OstreeManager {
  public:
   DockerAppManager(PackageConfig pconfig, std::shared_ptr<INvStorage> storage, std::shared_ptr<Bootloader> bootloader,
                    std::shared_ptr<HttpInterface> http)
-      : OstreeManager(pconfig, storage, bootloader, http) {
+      : OstreeManager(std::move(pconfig), std::move(storage), std::move(bootloader), std::move(http)) {
     fake_fetcher_ = std::make_shared<Uptane::Fetcher>(Config(), http_);
   }
   bool fetchTarget(const Uptane::Target &target, Uptane::Fetcher &fetcher, const KeyManager &keys,
-                   FetcherProgressCb progress_cb, const api::FlowControlToken *token = nullptr) override;
+                   FetcherProgressCb progress_cb, const api::FlowControlToken *token) override;
   data::InstallationResult install(const Uptane::Target &target) const override;
   std::string name() const override { return "ostree+docker-app"; }
 
  private:
-  bool iterate_apps(const Uptane::Target &target, DockerAppCb cb) const;
+  bool iterate_apps(const Uptane::Target &target, const DockerAppCb &cb) const;
 
   // iterate_apps needs an Uptane::Fetcher. However, its an unused parameter
   // and we just need to construct a dummy one to make the compiler happy.
