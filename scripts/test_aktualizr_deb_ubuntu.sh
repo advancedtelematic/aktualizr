71c4eb71ff984a24c8c75d95c66d5390ae5da325
Zee314159 <252806294@qq.com>
2019-09-17 09:27:04 +0200

    Change selfupdate to use fake package manager
    Signed-off-by: Zee314159 <252806294@qq.com>

--- scripts/test_aktualizr_deb_ubuntu.sh
+++ scripts/test_aktualizr_deb_ubuntu.sh
@@ -9,4 +9,4 @@ IMG_TAG=aktualizr-deb-$(cat /proc/sys/kernel/random/uuid)
 echo "Building docker for testing aktualizr deb package inside it."
 docker build -t "${IMG_TAG}" -f "${INSTALL_DOCKERFILE}" .
 echo "Running docker container with aktualizr debian package inside."
-docker run --rm -v "${PKG_SRCDIR}":/persistent -t "${IMG_TAG}" /bin/bash -c "/scripts/test_install_aktualizr_and_update.sh && /scripts/test-uptane-generator.sh"
+docker run --rm -v "${PKG_SRCDIR}":/persistent -t "${IMG_TAG}" /bin/bash -c "/scripts/test-uptane-generator.sh"
---
+++ tests/test_install_aktualizr_and_update.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+set -exuo pipefail
+SCRIPT_DIR="$(dirname "$0")"
+TEST_INSTALL_DESTDIR=$(mktemp -d)/install
+./scripts/selfupdate_server.py 0 "$TEST_INSTALL_DESTDIR"&
+PORT=$("$SCRIPT_DIR/find_listening_port.sh" $!)
+trap 'kill %1' EXIT
+
+until curl 127.0.0.1:"$PORT" &> /dev/null; do
+  sleep 0.2
+done
+
+$1/src/aktualizr_primary/aktualizr --version
+
+$1/src/uptane_generator/uptane-generator generate --path "$TEST_INSTALL_DESTDIR/fake_root"
+$1/src/uptane_generator/uptane-generator image --path "$TEST_INSTALL_DESTDIR/fake_root" --targetname selfupdate_2.0 --filename "./tests/test_data/selfupdate_2.0" --hwid selfupdate-device
+$1/src/uptane_generator/uptane-generator addtarget --path "$TEST_INSTALL_DESTDIR/fake_root" --targetname selfupdate_2.0 --hwid selfupdate-device --serial 723f79763eda1c753ce565c16862c79acdde32eb922d6662f088083c51ffde66
+$1/src/uptane_generator/uptane-generator signtargets --path "$TEST_INSTALL_DESTDIR/fake_root"
+
+TEMP_DIR=$(mktemp -d)
+mkdir -m 700 -p "$TEMP_DIR/import"
+cp ./tests/test_data/prov_selfupdate/* "$TEMP_DIR/import"
+echo -e "[storage]\\npath = \"$TEMP_DIR\"\\n[import]\\nbase_path = \"$TEMP_DIR/import\"" > "$TEMP_DIR/conf.toml"
+echo -e "[tls]\\nserver = \"http://localhost:$PORT\"" >> "$TEMP_DIR/conf.toml"
+$1/src/aktualizr_primary/aktualizr -c ./tests/config/selfupdate.toml -c "$TEMP_DIR/conf.toml" once
+
+# check the updated file appeared in the installation directory and sha256sum matches expectation
+TEST_INSTALL_IMAGE_DIR=$TEMP_DIR/images
+if [ ! -d $TEST_INSTALL_IMAGE_DIR ]; then
+    echo "$TEST_INSTALL_IMAGE_DIR does not exist"
+    exit 1
+else
+    cd $TEST_INSTALL_IMAGE_DIR
+    for filename in `ls`; do
+        checksum=$(echo $(sha256sum $filename) | cut -f 1 -d " " | tr [a-f] [A-F] )
+        if [ "$checksum" != "$filename" ]; then
+            echo "ERROR: sha256sum does not match."
+            exit 1
+        fi
+    done
+fi
