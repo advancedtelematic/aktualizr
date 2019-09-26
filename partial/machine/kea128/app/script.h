1d07f5d008efea0fb99a1f606353ccea1ca2dd9d
Laurent Bonnans <laurent.bonnans@here.com>
2019-09-26 15:15:54 +0200

    Move out uptiny from aktualizr's main repository
    Signed-off-by: Laurent Bonnans <laurent.bonnans@here.com>

--- partial/machine/kea128/app/script.h
+++
@@ -1,7 +0,0 @@
-#ifndef ATS_MS1_SCRIPT_H
-#define ATS_MS1_SCRIPT_H
-
-void script_init(void);
-void script_execute(void);
-
-#endif /* ATS_MS1_SCRIPT_H */
--- partial/machine/kea128/drivers/include/headlight.h
+++
@@ -1,7 +0,0 @@
-#ifndef ATS_DRIVERS_HEADLIGHT_H
-#define ATS_DRIVERS_HEADLIGHT_H
-
-void headlight_init(void);
-void headlight_set(int num, int val);
-
-#endif
