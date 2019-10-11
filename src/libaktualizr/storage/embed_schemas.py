advancedtelematic/aktualizr

commit ca3c1ed7a30fda5ffe5b3dff716d5cb8b9f66806

Remove unexisting CMake target
The `sql_schemas_target` is not generated anymore,
but was still present in the OUTPUT section of
`add_custom_command` rule. That was causing
`make` to rebuild the target every time.

Signed-off-by: Eugene Smirnov <evgenii.smirnov@here.com>

src/libaktualizr/storage/CMakeLists.txt
@@ -1,4 +1,4 @@
-add_custom_command(OUTPUT sql_schemas.cc sql_schemas_target
+add_custom_command(OUTPUT sql_schemas.cc
     COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/embed_schemas.py ${PROJECT_SOURCE_DIR}/config/sql/ ${CMAKE_CURRENT_BINARY_DIR}/sql_schemas.cc libaktualizr
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
 )

commit 606fea02338d04a3585472ac697e623ddbee671a

Fix the CMake SQL schema generation rule
It should always run, as the script itself avoids useless rewrites.

Was broken since ca3c1ed7a30fda5ffe5b3dff716d5cb8b9f66806

Signed-off-by: Laurent Bonnans <laurent.bonnans@here.com>

src/libaktualizr/storage/CMakeLists.txt
@@ -1,4 +1,7 @@
-add_custom_command(OUTPUT sql_schemas.cc
+# note: the dummy `sql_schemas_target` is here on purpose, to cause the command
+# to always be run. It's smart enough not to rewrite `sql_schemas.cc` and cause
+# a global rebuild if nothing was changed.
+add_custom_command(OUTPUT sql_schemas.cc sql_schemas_target
     COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/embed_schemas.py ${PROJECT_SOURCE_DIR}/config/sql/ ${CMAKE_CURRENT_BINARY_DIR}/sql_schemas.cc libaktualizr
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
 )

src/libaktualizr/storage/embed_schemas.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
+import io
 import sys
 import os
-import re
 
-warning_text = '''
-//This is autogenerated file, don't modify it manually
-//Latest schema timestamp: %d
+heading_text = '''
+// This is file was autogenerated, do not modify it manually
 #include <string>
 #include <vector>
 #include <map>
 '''
+
 
 def escape_string(sql):
     return sql.translate(str.maketrans({'"':  r'\"', '\n':  '\\n'}))
@@ -26,7 +26,8 @@ def apend_migration(migration_path, header_file):
 if __name__ == '__main__':
     if len(sys.argv) != 4:
         print("\nIncorrect arguments")
-        print("Usage:\n {} {} {} {}\n".format(sys.argv[0], "folder_with_schemas", "generated_file_path", "name_prefix"))
+        print("Usage:\n {} {} {} {}\n".format(sys.argv[0], "folder_with_schemas",
+                                              "generated_file_path", "name_prefix"))
         sys.exit(-1)
 
     sql_folder = sys.argv[1]
@@ -37,43 +38,41 @@ if __name__ == '__main__':
     migration_list = sorted(os.listdir(migration_folder))
     rollback_migrations_list = sorted(os.listdir(rollback_folder))
 
-    file_depends_list = [os.path.join(sql_folder, "schema.sql")]
-    file_depends_list += [os.path.join(migration_folder, p) for p in migration_list]
-    file_depends_list += [os.path.join(rollback_folder, p) for p in rollback_migrations_list]
-    file_depends_list.append(__file__)  # set dependency on itself (this script)
-    max_file_stamp = max(os.path.getmtime(p) for p in file_depends_list)
-
-    pattern = re.compile("//Latest schema timestamp: (\d+)")
-    if os.path.exists(schemas_header):
-        for i, line in enumerate(open(schemas_header)):
-            for match in re.finditer(pattern, line):
-                if int(match.groups()[0]) == int(max_file_stamp):
-                    # header up to date, exiting
-                    sys.exit(0)
-
-    with open(schemas_header, 'w') as header_file:
-        header_file.write(warning_text % max_file_stamp)
-        header_file.write("extern const std::vector<std::string> {}_schema_migrations = {{".format(prefix))
+    with io.BytesIO() as outf:
+        out_text = io.TextIOWrapper(outf, write_through=True)
+        out_text.write(heading_text)
+        out_text.write("extern const std::vector<std::string> {}_schema_migrations = {{".format(prefix))
         for migration in migration_list[:-1]:
-            apend_migration(os.path.join(migration_folder, migration), header_file)
-            header_file.write("\",\n")
-        apend_migration(os.path.join(migration_folder, migration_list[-1]), header_file)
-        header_file.write("\"\n};\n")
+            apend_migration(os.path.join(migration_folder, migration), out_text)
+            out_text.write("\",\n")
+        apend_migration(os.path.join(migration_folder, migration_list[-1]), out_text)
+        out_text.write("\"\n};\n")
 
-        header_file.write("extern const std::vector<std::string> {}_schema_rollback_migrations = {{".format(prefix))
+        out_text.write("extern const std::vector<std::string> {}_schema_rollback_migrations = {{".format(prefix))
         if len(rollback_migrations_list) > 0:
             ver = int(rollback_migrations_list[0].split(".")[1])
             for i in range(ver):
-                header_file.write("\"\",\n")
+                out_text.write("\"\",\n")
             for migration in rollback_migrations_list[:-1]:
-                apend_migration(os.path.join(rollback_folder, migration), header_file)
-                header_file.write("\",\n")
+                apend_migration(os.path.join(rollback_folder, migration), out_text)
+                out_text.write("\",\n")
             version = int(rollback_migrations_list[-1].split(".")[1])
-            apend_migration(os.path.join(rollback_folder, rollback_migrations_list[-1]), header_file)
-            header_file.write("\"")
-        header_file.write("\n};\n")
+            apend_migration(os.path.join(rollback_folder, rollback_migrations_list[-1]), out_text)
+            out_text.write("\"")
+        out_text.write("\n};\n")
 
         current_schema = open(os.path.join(sql_folder, "schema.sql"), 'r').read()
         current_schema_escaped = escape_string(current_schema)
-        header_file.write('extern const std::string %s_current_schema = "%s";' % (prefix, current_schema_escaped));
-        header_file.write('extern const int %s_current_schema_version = %u;' % (prefix ,(len(migration_list)-1)));
+        out_text.write('extern const std::string %s_current_schema = "%s";' % (prefix, current_schema_escaped));
+        out_text.write('extern const int %s_current_schema_version = %u;' % (prefix ,(len(migration_list)-1)));
+
+        # compare with current and only overwrite if it was changed
+        new_content = outf.getvalue()
+        try:
+            with open(schemas_header, 'r') as hf:
+                current_content = hf.read().encode()
+        except Exception:
+            current_content = b''
+        if new_content != current_content:
+            with open(schemas_header, 'wb') as hf:
+                hf.write(new_content)