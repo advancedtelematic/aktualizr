6a160517f8ca42c456250384fdba3d36b72ef99d
Laurent Bonnans <laurent.bonnans@here.com>
2019-10-17 12:32:18 +0200

    Avoid copying the repository when making tarballs
    By using our fork of git-archive-all, until our changes are hopefully
    merged upstream.
    
    Signed-off-by: Laurent Bonnans <laurent.bonnans@here.com>

--- scripts/make_src_archive.sh
+++ scripts/make_src_archive.sh
@@ -1,26 +1,25 @@
 #!/usr/bin/env bash
 
-set -eu
+set -eux
 
 SCRIPTS_DIR=$(readlink -f "$(dirname "$0")")
 
-OUTPUT=$1
-REPO=${2:-.}
+OUTPUT=$(realpath "$1")
+REPO=$(realpath "${2:-.}")
 
 python3 -m venv venv
 
 . venv/bin/activate
 
-pip install 'git_archive_all==1.19.4'
-
-ABS_OUT=$(realpath "$OUTPUT")
+# Use our fork which makes it easier to add extra files
+# pip install 'git_archive_all==1.19.4'
+pip install git+https://github.com/lbonn/git-archive-all.git
 
 TMPDIR=$(mktemp -d)
 trap 'rm -rf $TMPDIR' EXIT
-cp -r "$REPO" "$TMPDIR/aktualizr"
+cd "$TMPDIR"
 
-cd "$TMPDIR/aktualizr"
-# include version in tarball
-"$SCRIPTS_DIR/get_version.sh" > VERSION
+# store version in archive
+"$SCRIPTS_DIR/get_version.sh" git "$REPO" > VERSION
 
-git-archive-all "$ABS_OUT" --extra=VERSION
+git-archive-all -C "$REPO" --extra VERSION "$OUTPUT"
