cf01e89a43e515cdeae565daf953b55d0af11fed
Patrick Vacek <patrickvacek@gmail.com>
2019-12-27 12:59:54 +0100

    Fix Secondary root metadata fetching logic.
    It now needs to respect versions.
    
    Signed-off-by: Patrick Vacek <patrickvacek@gmail.com>

--- src/aktualizr_secondary/aktualizr_secondary_metadata.cc
+++ src/aktualizr_secondary/aktualizr_secondary_metadata.cc
@@ -8,24 +8,26 @@ Metadata::Metadata(const Uptane::RawMetaPack& meta_pack)
           {Uptane::Role::TIMESTAMP, meta_pack.image_timestamp},
           {Uptane::Role::SNAPSHOT, meta_pack.image_snapshot},
           {Uptane::Role::TARGETS, meta_pack.image_targets},
-      } {}
+      } {
+  director_root_version = Uptane::Version(Uptane::extractVersionUntrusted(meta_pack.director_root));
+  image_root_version = Uptane::Version(Uptane::extractVersionUntrusted(meta_pack.image_root));
+}
 
 bool Metadata::fetchRole(std::string* result, int64_t maxsize, Uptane::RepositoryType repo, const Uptane::Role& role,
                          Uptane::Version version) const {
   (void)maxsize;
-  (void)version;
 
-  return getRoleMetadata(result, repo, role);
+  return getRoleMetadata(result, repo, role, version);
 }
 
 bool Metadata::fetchLatestRole(std::string* result, int64_t maxsize, Uptane::RepositoryType repo,
                                const Uptane::Role& role) const {
   (void)maxsize;
-  return getRoleMetadata(result, repo, role);
+  return getRoleMetadata(result, repo, role, Uptane::Version());
 }
 
-bool Metadata::getRoleMetadata(std::string* result, const Uptane::RepositoryType& repo,
-                               const Uptane::Role& role) const {
+bool Metadata::getRoleMetadata(std::string* result, const Uptane::RepositoryType& repo, const Uptane::Role& role,
+                               Uptane::Version version) const {
   const std::unordered_map<std::string, std::string>* metadata_map = nullptr;
 
   if (repo == Uptane::RepositoryType::Director()) {
@@ -43,6 +45,18 @@ bool Metadata::getRoleMetadata(std::string* result, const Uptane::RepositoryType
   if (found_meta_it == metadata_map->end()) {
     LOG_ERROR << "There are no any metadata for the given type of role: " << repo.toString() << ": " << role.ToString();
     return false;
+  }
+
+  if (role == Uptane::Role::Root() && version != Uptane::Version()) {
+    if (repo == Uptane::RepositoryType::Director() && director_root_version != version) {
+      LOG_DEBUG << "Requested Director root version " << version << " but only version " << director_root_version
+                << " is available.";
+      return false;
+    } else if (repo == Uptane::RepositoryType::Image() && image_root_version != version) {
+      LOG_DEBUG << "Requested Image repo root version " << version << " but only version " << image_root_version
+                << " is available.";
+      return false;
+    }
   }
 
   *result = found_meta_it->second;
