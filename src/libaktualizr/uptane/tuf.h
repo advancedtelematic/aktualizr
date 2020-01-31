7ab9968367fcdc0cd77ea6d2c2781830d10aab26
Andy Doan <andy@foundries.io>
2020-01-31 10:03:34 -0600

    Address comment by Mike
    Signed-off-by: Andy Doan <andy@foundries.io>

--- src/libaktualizr/uptane/tuf.h
+++ src/libaktualizr/uptane/tuf.h
@@ -262,9 +262,7 @@ class Target {
   void setUri(std::string uri) { uri_ = std::move(uri); };
   bool MatchHash(const Hash &hash) const;
 
-  void InsertEcu(const EcuSerial &ecuIdentifier, const HardwareIdentifier &hwid) {
-    ecus_.insert({ecuIdentifier, hwid});
-  }
+  void InsertEcu(const std::pair<EcuSerial, HardwareIdentifier> &pair) { ecus_.insert(pair); }
 
   bool IsForEcu(const EcuSerial &ecuIdentifier) const {
     return (std::find_if(ecus_.cbegin(), ecus_.cend(), [&ecuIdentifier](std::pair<EcuSerial, HardwareIdentifier> pair) {
