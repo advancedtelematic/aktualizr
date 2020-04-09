advancedtelematic/aktualizr

commit 48613a90263ee2b022e4568abf157157e4beab81

packagemanager: Clean up unused images after install
Docker builds up old images and volumes over time consuming disk space.
This is normally not what you want for a managed device.

Signed-off-by: Andy Doan <andy@foundries.io>
Signed-off-by: Michael Scott <mike@foundries.io>

src/libaktualizr/package_manager/dockerappmanager.cc
@@ -25,4 +25,8 @@ DockerAppManagerConfig::DockerAppManagerConfig(const PackageConfig &pconfig) {
   if (raw.count("docker_compose_bin") == 1) {
     docker_compose_bin = raw.at("docker_compose_bin");
   }
+
+  if (raw.count("docker_prune") == 1) {
+    docker_compose_bin = raw.at("docker_prune");
+  }
 }

src/libaktualizr/package_manager/docker_fake.sh
@@ -58,5 +58,10 @@ if [ "$1" = "down" ] ; then
   touch ../docker-compose-down-called
   exit 0
 fi
+if [ "$1" = "image" ] ; then
+  if [ "$2" = "prune" ] ; then
+    exit 0
+  fi
+fi
 echo "Unknown command: $*"
 exit 1

src/libaktualizr/package_manager/dockerapp_bundles.cc
@@ -135,6 +135,16 @@ data::InstallationResult DockerAppBundles::install(const Uptane::Target &target)
       res = data::InstallationResult(data::ResultCode::Numeric::kInstallFailed, "Could not install docker app");
     }
   };
+
+  if (dappcfg_.docker_prune) {
+    LOG_INFO << "Pruning unused docker images";
+    // Utils::shell which isn't interactive, we'll use std::system so that
+    // stdout/stderr is streamed while docker sets things up.
+    if (std::system("docker image prune -a -f") != 0) {
+      LOG_WARNING << "Unable to prune unused docker images";
+    }
+  }
+
   return res;
 }
 

src/libaktualizr/package_manager/dockerapp_standalone.cc
@@ -167,7 +167,16 @@ data::InstallationResult DockerAppStandalone::install(const Uptane::Target &targ
     return DockerApp(app, config).start();
   };
   if (!iterate_apps(target, cb)) {
-    return data::InstallationResult(data::ResultCode::Numeric::kInstallFailed, "Could not render docker app");
+    res = data::InstallationResult(data::ResultCode::Numeric::kInstallFailed, "Could not render docker app");
+  }
+
+  if (dappcfg_.docker_prune) {
+    LOG_INFO << "Pruning unused docker images";
+    // Utils::shell which isn't interactive, we'll use std::system so that
+    // stdout/stderr is streamed while docker sets things up.
+    if (std::system("docker image prune -a -f") != 0) {
+      LOG_WARNING << "Unable to prune unused docker images";
+    }
   }
   return res;
 }

src/libaktualizr/package_manager/dockerappmanager.h
@@ -15,6 +15,7 @@ class DockerAppManagerConfig {
   boost::filesystem::path docker_app_params;
   boost::filesystem::path docker_app_bin{"/usr/bin/docker-app"};
   boost::filesystem::path docker_compose_bin{"/usr/bin/docker-compose"};
+  bool docker_prune{true};
 };
 
 class DockerAppStandalone : public OstreeManager {

commit e71f68961c78a9def75be08aed22780a57c01a16

pacman: Fix docker prune configuration handling
Fix a copy paste bug introduced by:

 https://github.com/advancedtelematic/aktualizr/commit/48613a90263ee2b022e4568abf157157e4beab81

Signed-off-by: Andy Doan <andy@foundries.io>

src/libaktualizr/package_manager/dockerappmanager.cc
@@ -27,6 +27,8 @@ DockerAppManagerConfig::DockerAppManagerConfig(const PackageConfig &pconfig) {
   }
 
   if (raw.count("docker_prune") == 1) {
-    docker_compose_bin = raw.at("docker_prune");
+    std::string val = raw.at("docker_prune");
+    boost::algorithm::to_lower(val);
+    docker_prune = val != "0" && val != "false";
   }
 }

src/libaktualizr/package_manager/dockerappmanager_test.cc
@@ -5,6 +5,7 @@
 
 #include "config/config.h"
 #include "http/httpclient.h"
+#include "package_manager/dockerappmanager.h"
 #include "package_manager/packagemanagerfactory.h"
 #include "package_manager/packagemanagerinterface.h"
 #include "primary/sotauptaneclient.h"
@@ -41,6 +42,34 @@ TEST(DockerAppManager, PackageManager_Factory_Good) {
   std::shared_ptr<INvStorage> storage = INvStorage::newStorage(config.storage);
   auto pacman = PackageManagerFactory::makePackageManager(config.pacman, config.bootloader, storage, nullptr);
   EXPECT_TRUE(pacman);
+}
+
+TEST(DockerAppManager, DockerAppConfig) {
+  Config config;
+  config.pacman.type = PACKAGE_MANAGER_OSTREEDOCKERAPP;
+  config.pacman.sysroot = test_sysroot.string();
+  config.pacman.ostree_server = treehub_server;
+  config.pacman.extra["docker_apps_root"] = "apps-root";
+  config.pacman.extra["docker_apps"] = "app1 app2";
+  config.pacman.extra["docker_app_bin"] = "dab";
+  config.pacman.extra["docker_compose_bin"] = "compose";
+
+  DockerAppManagerConfig dacfg(config.pacman);
+  ASSERT_TRUE(dacfg.docker_prune);
+  ASSERT_EQ(2, dacfg.docker_apps.size());
+  ASSERT_EQ("app1", dacfg.docker_apps[0]);
+  ASSERT_EQ("app2", dacfg.docker_apps[1]);
+  ASSERT_EQ("apps-root", dacfg.docker_apps_root);
+  ASSERT_EQ("dab", dacfg.docker_app_bin);
+  ASSERT_EQ("compose", dacfg.docker_compose_bin);
+
+  config.pacman.extra["docker_prune"] = "0";
+  dacfg = DockerAppManagerConfig(config.pacman);
+  ASSERT_FALSE(dacfg.docker_prune);
+
+  config.pacman.extra["docker_prune"] = "FALSE";
+  dacfg = DockerAppManagerConfig(config.pacman);
+  ASSERT_FALSE(dacfg.docker_prune);
 }
 
 TEST(DockerAppManager, DockerAppStandalone) {