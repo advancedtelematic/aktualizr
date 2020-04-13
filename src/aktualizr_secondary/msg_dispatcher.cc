77a4e16a833379267bc3143ccef71cb6f2df5b0d
Mykhaylo Sul <myk.sul@gmail.com>
2020-04-13 17:41:05 +0300

    OTA-4814: set  level for request/response msg logging
    Signed-off-by: Mykhaylo Sul <myk.sul@gmail.com>

--- src/aktualizr_secondary/msg_dispatcher.cc
+++ src/aktualizr_secondary/msg_dispatcher.cc
@@ -10,15 +10,15 @@ void MsgDispatcher::registerHandler(AKIpUptaneMes_PR msg_id, Handler handler) {
 }
 
 MsgDispatcher::HandleStatusCode MsgDispatcher::handleMsg(const Asn1Message::Ptr& in_msg, Asn1Message::Ptr& out_msg) {
-  LOG_INFO << "Got a request message from Primary: " << in_msg->toStr();
+  LOG_TRACE << "Got a request message from Primary: " << in_msg->toStr();
 
   auto find_res_it = handler_map_.find(in_msg->present());
   if (find_res_it == handler_map_.end()) {
     return MsgDispatcher::HandleStatusCode::kUnkownMsg;
   }
-  LOG_INFO << "Found a handler for the request message, processing it...";
+  LOG_TRACE << "Found a handler for the request message, processing it...";
   auto handle_status_code = find_res_it->second(*in_msg, *out_msg);
-  LOG_INFO << "Got a response message from a handler: " << out_msg->toStr();
+  LOG_TRACE << "Got a response message from a handler: " << out_msg->toStr();
 
   return handle_status_code;
 }
