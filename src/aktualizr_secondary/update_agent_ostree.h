2c3d7cd63cade234c7702469ae24f73c685cf785
Mike Sul <ext-mykhaylo.sul@here.com>
2020-04-14 18:21:36 +0300

    OTA-4814: move prefixed underscore to suffix for member variables.
    Now aligns with the code guidelines.
    
    Signed-off-by: Mike Sul <ext-mykhaylo.sul@here.com>
    Signed-off-by: Patrick Vacek <patrickvacek@gmail.com>

--- src/aktualizr_secondary/update_agent_ostree.h
+++ src/aktualizr_secondary/update_agent_ostree.h
@@ -10,10 +10,10 @@ class OstreeUpdateAgent : public UpdateAgent {
  public:
   OstreeUpdateAgent(const boost::filesystem::path& sysroot_path, std::shared_ptr<KeyManager>& key_mngr,
                     std::shared_ptr<OstreeManager>& ostree_pack_man, std::string targetname_prefix)
-      : _sysrootPath(sysroot_path),
-        _keyMngr(key_mngr),
-        _ostreePackMan(ostree_pack_man),
-        _targetname_prefix(std::move(targetname_prefix)) {}
+      : sysrootPath_(sysroot_path),
+        keyMngr_(key_mngr),
+        ostreePackMan_(ostree_pack_man),
+        targetname_prefix_(std::move(targetname_prefix)) {}
 
  public:
   bool isTargetSupported(const Uptane::Target& target) const override;
@@ -24,10 +24,10 @@ class OstreeUpdateAgent : public UpdateAgent {
   data::InstallationResult applyPendingInstall(const Uptane::Target& target) override;
 
  private:
-  const boost::filesystem::path& _sysrootPath;
-  std::shared_ptr<KeyManager> _keyMngr;
-  std::shared_ptr<OstreeManager> _ostreePackMan;
-  const ::std::string _targetname_prefix;
+  const boost::filesystem::path& sysrootPath_;
+  std::shared_ptr<KeyManager> keyMngr_;
+  std::shared_ptr<OstreeManager> ostreePackMan_;
+  const ::std::string targetname_prefix_;
 };
 
 #endif  // AKTUALIZR_SECONDARY_UPDATE_AGENT_OSTREE_H
