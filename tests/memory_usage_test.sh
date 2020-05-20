14b7e2ebb3f85b26cd674e8723535d730dad197a
Laurent Bonnans <laurent.bonnans@here.com>
2020-05-20 11:50:04 +0200

    Clean up memory_usage_test.sh a bit
    Signed-off-by: Laurent Bonnans <laurent.bonnans@here.com>
    Signed-off-by: Patrick Vacek <patrickvacek@gmail.com>

--- tests/memory_usage_test.sh
+++ tests/memory_usage_test.sh
@@ -1,20 +1,24 @@
 #!/bin/bash -e
 
 TEMP_DIR=$(mktemp -d)/aktualizr
-MEMMORY_LIMIT=10485760 #10 Mib
+MEMORY_LIMIT=10485760 #10 Mib
 
 trap 'kill %1; rm -rf $TEMP_DIR' EXIT
 
-PATH=$PATH:"$1/src/uptane_generator/"
-./src/uptane_generator/run/create_repo.sh $TEMP_DIR localhost
+AKTUALIZR=$1
+UPTANE_GENERATOR=$2
+BUILD_DIR=$3
+PATH="$(dirname "$UPTANE_GENERATOR"):$PATH"
+./src/uptane_generator/run/create_repo.sh "$TEMP_DIR" localhost
 
+METRICS_FILE="$BUILD_DIR/metrics.txt"
 TARGET_SIZE="200M"
 
-mkdir -p $TEMP_DIR/deb/DEBIAN
-mkdir -p $TEMP_DIR/deb/usr/share
-fallocate -l $TARGET_SIZE $TEMP_DIR/deb/usr/share/target1
+mkdir -p "$TEMP_DIR/deb/DEBIAN"
+mkdir -p "$TEMP_DIR/deb/usr/share"
+fallocate -l $TARGET_SIZE "$TEMP_DIR/deb/usr/share/target1"
 
-cat > $TEMP_DIR/deb/DEBIAN/control <<EOF
+cat > "$TEMP_DIR/deb/DEBIAN/control" <<EOF
 Package: testdeb
 Version: 1.1-1
 Section: base
@@ -25,24 +29,32 @@ Description: Some fake package
  This is fake package.
 EOF
 
-dpkg-deb -Znone -b $TEMP_DIR/deb $TEMP_DIR/good.deb
-PATH="tests/test_data/fake_dpkg":$PATH
-$1/src/uptane_generator/uptane-generator image --path $TEMP_DIR/uptane --filename $TEMP_DIR/good.deb --targetname good.deb --hwid desktop
-$1/src/uptane_generator/uptane-generator addtarget --path $TEMP_DIR/uptane --targetname good.deb --hwid desktop --serial serial1
-$1/src/uptane_generator/uptane-generator signtargets --path $TEMP_DIR/uptane
-
+dpkg-deb -Znone -b "$TEMP_DIR/deb" "$TEMP_DIR/good.deb"
+PATH="tests/test_data/fake_dpkg:$PATH"
+"$UPTANE_GENERATOR" image --path "$TEMP_DIR/uptane" --filename "$TEMP_DIR/good.deb" --targetname good.deb --hwid desktop
+"$UPTANE_GENERATOR" addtarget --path "$TEMP_DIR/uptane" --targetname good.deb --hwid desktop --serial serial1
+"$UPTANE_GENERATOR" signtargets --path "$TEMP_DIR/uptane"
 
 sed -i 's/\[provision\]/\[provision\]\nprimary_ecu_serial = serial1/g'  "$TEMP_DIR/sota.toml"
 sed -i 's/type = "none"/type = "debian"/g' "$TEMP_DIR/sota.toml"
 
 ./src/uptane_generator/run/serve_repo.py 9000 "$TEMP_DIR" &
 
-valgrind --tool=massif --massif-out-file=$1/massif.out $1/src/aktualizr_primary/aktualizr --loglevel 1 --config "$TEMP_DIR/sota.toml" --run-mode once
+valgrind --tool=massif --massif-out-file="$BUILD_DIR/massif.out" "$AKTUALIZR" --loglevel 1 --config "$TEMP_DIR/sota.toml" --run-mode once
 
-HEAP_MEMMORY=$(grep -oP 'mem_heap_B=\K.*' $1/massif.out | sort -nr | head -n 1)
+HEAP_MEMORY=$(grep -oP 'mem_heap_B=\K.*' "$BUILD_DIR/massif.out" | sort -nr | head -n 1)
 echo -n "Heap memory usage is "
-echo $HEAP_MEMMORY | numfmt --to=iec-i
-if [ "$HEAP_MEMMORY" -gt "$MEMMORY_LIMIT" ]; then
-    echo "Test fail because of memory consumption is greater then $MEMMORY_LIMIT bytes"
+echo "$HEAP_MEMORY" | numfmt --to=iec-i
+
+line="massif_install_mem_peak $HEAP_MEMORY"
+if grep -q '^massif_install_mem' "$METRICS_FILE" 2> /dev/null; then
+    sed -i 's/^massif_install_mem.*$/'"$line" "$METRICS_FILE"
+else
+    echo "$line" >> "$METRICS_FILE"
+
+fi
+
+if [ "$HEAP_MEMORY" -gt "$MEMORY_LIMIT" ]; then
+    echo "Test fail because of memory consumption is greater then $MEMORY_LIMIT bytes"
     exit 1
 fi
