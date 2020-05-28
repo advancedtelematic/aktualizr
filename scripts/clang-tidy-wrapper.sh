74164ede2a783687c247527f90c03b5c8d743802
Patrick Vacek <patrickvacek@gmail.com>
2020-05-28 09:34:19 +0200

    Filter out files from clang-tidy that we aren't trying to compile.
    clang-tidy-6 and earlier skipped them automatically, but 8 does not. Now
    we do it ourselves.
    
    Signed-off-by: Patrick Vacek <patrickvacek@gmail.com>

---
+++ scripts/clang-tidy-wrapper.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+set -euo pipefail
+
+CLANG_TIDY="${1}"
+CMAKE_BINARY_DIR="${2}"
+CMAKE_SOURCE_DIR="${3}"
+FILE="${4}"
+
+if [[ ! -e "${CMAKE_BINARY_DIR}/compile_commands.json" ]]; then
+  echo "compile_commands.json not found!"
+  exit 1
+fi
+
+# Filter out anything that we aren't trying to compile. Older clang-tidy
+# versions (<=6) skipped them automatically.
+if grep "${FILE}" "${CMAKE_BINARY_DIR}/compile_commands.json"; then
+  ${CLANG_TIDY} -quiet -header-filter="\(${CMAKE_SOURCE_DIR}|\\.\\.\)/src/.*" --extra-arg-before=-Wno-unknown-warning-option -format-style=file -p "${CMAKE_BINARY_DIR}" "${FILE}"
+else
+  echo "Skipping ${FILE}"
+fi
