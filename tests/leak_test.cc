fa0bf2e55b9d9be4674eda1ea799016a49d3ec84
Patrick Vacek <patrickvacek@gmail.com>
2020-06-29 10:45:32 +0200

    Fix the leak test (so that it keeps leaking).
    It appears that gcc 9 fixes the leak automatically, so valgrind doesn't
    detect it. Now we actually use the leaky variable, which seems to do the
    trick.
    
    Signed-off-by: Patrick Vacek <patrickvacek@gmail.com>

--- tests/leak_test.cc
+++ tests/leak_test.cc
@@ -3,10 +3,14 @@
 /**
  * A test case that leaks memory, to check that we can spot this in valgrind
  */
-TEST(Leak, ThisTestLeaks) { EXPECT_TRUE(new int[45]); }
+TEST(Leak, ThisTestLeaks) {
+  int* temp = new int[45];
+  int temp2 = *temp++;
+  std::cout << temp2;
+}
 
 #ifndef __NO_MAIN__
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
